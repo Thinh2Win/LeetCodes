2b4a0c6c3d09e2f4806b1f1cd60f11bba9d7ee7d
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-06-08 14:49:23 -0700

    Time: 73 ms (77.74%), Space: 44.9 MB (71.33%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -6,11 +6,11 @@
 var search = function(nums, target) {
       let lo = 0, hi = nums.length-1;
     while (lo < hi) {
-        let mid = lo + Math.floor((hi-lo+1)/2);
-        if (target < nums[mid]) {
-            hi = mid - 1
+        let mid = lo + Math.floor((hi-lo)/2);
+        if (target > nums[mid]) {
+            lo = mid + 1;
         } else {
-            lo = mid; 
+            hi = mid; 
         }
     }
     return nums[lo]==target?lo:-1;
