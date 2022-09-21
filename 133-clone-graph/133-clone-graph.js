25bbc2304eb17416735732cc1e8050d26076ef22
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-09-21 13:45:20 -0700

    Time: 101 ms (44.91%), Space: 43.6 MB (79.95%) - LeetHub

--- 133-clone-graph/133-clone-graph.js
+++ 133-clone-graph/133-clone-graph.js
@@ -11,21 +11,19 @@
  * @return {Node}
  */
 var cloneGraph = function(node) {
+    if (!node) return null;
     let map = new Map();
     let start = node;
-    if (start === null) {
-        return null;
-    }
     let q = [start];
-    map.set(start, new Node(start.val));
-    while ( q.length > 0) {
+    map.set(start, new Node(start.val)); 
+    while (q.length > 0) {
         let currentNode = q.shift();
         currentNode.neighbors.forEach(neighbor => {
             if (!map.has(neighbor)) {
                 map.set(neighbor, new Node(neighbor.val));
-                q.push(neighbor); 
+                q.push(neighbor);
             }
-            map.get(currentNode).neighbors.push(map.get(neighbor)); 
+            map.get(currentNode).neighbors.push(map.get(neighbor));  
         });
     }
     return map.get(start);
