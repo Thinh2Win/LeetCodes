c5785d1575473aa77e1bfb7a9eb03bd025dc1ade
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-09-21 10:21:02 -0700

    Time: 443 ms (26.05%), Space: 68.7 MB (20.21%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.js
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.js
@@ -3,35 +3,16 @@
  * @param {number} k
  * @return {number[][]}
  */
-
-const findDistance = (point) => {
-    return (Math.pow(point[0], 2) + Math.pow(point[1], 2));
-}
-
-const quickSort = (array) => {
-    if (array.length === 1) {
-        return array;
-    }
-    let pivot = array[array.length - 1];
-    let left = [];
-    let right = [];
-        
-    for (let i = 0; i < array.length - 1; i += 1) {
-        if (findDistance(array[i]) < findDistance(pivot)) {
-            left.push(array[i]);
-        } else {
-            right.push(array[i]);
-        }
-    }
-    if (left.length > 0 && right.length > 0) {
-        return [...quickSort(left), pivot, ...quickSort(right)];
-    } else if (left.length === 0 && right.length > 0) {
-        return [pivot, ...quickSort(right)];
-    } else {
-        return [...quickSort(left), pivot];
-    }
-}
 var kClosest = function(points, k) {
-     points.sort((a,b) => (Math.pow(a[0], 2) + Math.pow(a[1], 2)) - (Math.pow(b[0], 2) + Math.pow(b[1], 2)))
-    return points.slice(0,k);
+    let answer = []; 
+    if (points.length === 0) {
+        return answer;
+    }
+    points.forEach(point => {
+        let distance = Math.sqrt(Math.pow(point[0], 2) + Math.pow(point[1], 2));
+        answer.push([distance, point]);
+    });
+    answer.sort((a, b) => a[0] - b[0]);
+    let sliced = answer.slice(0, k).map(point => point[1]);
+    return sliced;
 };
