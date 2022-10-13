e100242b40ac69d98fe83d237c20f442d73c47ab
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-10-13 09:44:58 -0700

    Time: 79 ms (88.14%), Space: 42.9 MB (74.65%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.js
+++ 0042-trapping-rain-water/0042-trapping-rain-water.js
@@ -11,16 +11,16 @@ var trap = function(height) {
     while (L < R) {
         leftMax = Math.max(height[L], leftMax);
         rightMax = Math.max(height[R], rightMax);
-        if (height[L] < leftMax) {
-            trappedWater += leftMax - height[L];
-        }
-         if (height[R] < rightMax) {
-            trappedWater += rightMax - height[R];
-        }
         if (height[L] < height[R]) {
             L += 1;
         } else {
             R -= 1;
+        }
+         if (height[L] < leftMax) {
+            trappedWater += leftMax - height[L];
+        }
+         if (height[R] < rightMax) {
+            trappedWater += rightMax - height[R];
         }
     }
     return trappedWater;
