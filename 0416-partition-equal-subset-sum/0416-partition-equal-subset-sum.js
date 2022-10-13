0e0f9db9e2c70606c595f96bca0025013dd8bc06
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-10-13 11:37:03 -0700

    Time: 1325 ms (18.48%), Space: 168.2 MB (8.29%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canPartition = function(nums) {
+      let sum = nums.reduce((acc, cur) => acc + cur);
+  if (sum % 2 === 1) {
+    return false;
+  }
+  sum /= 2;
+  let memo = new Map();
+  return dfs(nums, sum, 0, memo);
+};
+
+const dfs = (nums, sum, idx, memo) => {
+  if (sum === 0) {
+    return true;
+  }
+  if (sum < 0 || idx === nums.length) {
+    return false;
+  }
+  if (memo.has(`${idx}-${sum}`)) {
+    return memo.get(`${idx}-${sum}`);
+  }
+  // either include current element or skip
+  let res =
+    dfs(nums, sum - nums[idx], idx + 1, memo) || dfs(nums, sum, idx + 1, memo);
+  memo.set(`${idx}-${sum}`, res);
+  return res;
+};
