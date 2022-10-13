0f3e9984e98d34aec949862e41966854a60042c0
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-10-13 16:44:24 -0700

    Time: 429 ms (49.17%), Space: 55.2 MB (62.01%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.js
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.js
@@ -7,24 +7,12 @@ var canPartition = function(nums) {
     nums.forEach(num => target += num);
     if (target % 2 === 1) return false; 
     target /= 2;
-    let memo = new Map();
-    let foundTarget = false;
-    const DFS = (array, sum, idx) => {
-        if (sum === 0) {
-            foundTarget = true;
-            return;
-        }
-        if (foundTarget || idx === array.length - 1) {
-            return;
-        }
-        if (memo.has(`${sum},${idx}`) || sum < 0) {
-            return;
-        }
-        
-        DFS(array, sum - array[idx], idx + 1);
-        DFS(array, sum, idx + 1) ;
-        memo.set(`${sum},${idx}`, true);
-    }
-    DFS(nums, target, 0);
-    return foundTarget;
+    let combos = new Set();
+    combos.add(0);
+    nums.forEach(num => {
+        [...combos].forEach(number => {
+            combos.add(number + num);
+        });
+    });
+    return combos.has(target);
 };
