3ea2bd1c947f8ec691c508de18f4083b48344b6e
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-10-18 13:21:58 -0700

    Time: 80 ms (96.94%), Space: 52 MB (14.82%) - LeetHub

---
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxPathSum = function(root) {
+    let maxSum = -Infinity; 
+    const DFS = (currentNode) => {
+        let leftMax = currentNode.left ? DFS(currentNode.left) : 0;
+        let rightMax = currentNode.right ? DFS(currentNode.right) : 0;
+        maxSum = Math.max(maxSum, leftMax + currentNode.val, rightMax + currentNode.val, leftMax + rightMax + currentNode.val, currentNode.val);
+        return Math.max(leftMax + currentNode.val, rightMax + currentNode.val, currentNode.val);
+    }
+    maxSum = Math.max(DFS(root), maxSum);
+    return maxSum;
+};
