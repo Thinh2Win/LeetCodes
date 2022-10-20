a00d17454e056f73392f80cabfe83482261475d8
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-10-20 12:44:06 -0700

    Time: 231 ms (41.64%), Space: 47.5 MB (62.88%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.js
+++ 0012-integer-to-roman/0012-integer-to-roman.js
@@ -2,56 +2,15 @@
  * @param {number} num
  * @return {string}
  */
-
-let map = {
-    1: 'I',
-    5: 'V',
-    10: 'X',
-    50: 'L',
-    100: 'C',
-    500: 'D',
-    1000: 'M' 
-};
-
-let convert = {
-        4: 1000,
-        3: 100, 
-        2: 10, 
-        1: 1
-};
+const val = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
+const rom = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
 
 var intToRoman = function(num) {
     let answer = '';
-    let numbers = `${num}`;
-    for (let i = 0; i < numbers.length; i++) {
-        let currentSpot = numbers.length - i;
-        let count = +numbers[i];
-        if (+numbers[i] < 5) {
-            if (+numbers[i] === 4) {
-                answer += map[1 * convert[currentSpot]];
-                answer += map[5 * convert[currentSpot]];
-            } else {
-                 while(count > 0) {
-                answer += map[1 * convert[currentSpot]];
-                count -= 1;
-            }
-            }
-        }
-        if (+numbers[i] === 5) {
-            answer += map[5 * convert[currentSpot]];
-        }
-        if (+numbers[i] > 5) {
-            if (+numbers[i] === 9) {
-                answer += map[1 * convert[currentSpot]];
-                answer += map[10 * convert[currentSpot]];
-            } else {
-              answer += map[5 * convert[currentSpot]];
-            count -= 5;
-            while (count > 0) {
-                answer += map[1 * convert[currentSpot]];
-                count -= 1;
-                }   
-            }
+    for (let i = 0; i < val.length; i++) {
+        while (num >= val[i]) {
+            answer += rom[i];
+            num -= val[i];
         }
     }
     return answer;
