693f4c996aa42885cacaec09d791cddd468aaf58
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-10-20 16:58:44 -0700

    Time: 82 ms (82.59%), Space: 43.2 MB (29.48%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.js
+++ 0091-decode-ways/0091-decode-ways.js
@@ -3,25 +3,24 @@
  * @return {number}
  */
 var numDecodings = function(s) {
-   if (s.length < 1) return 0;
-    let memo = [];
-    var recur = function (index) {
-        let result = 0
-        if (index == s.length) return 1; // base case 1: reach the end = correct path
-        if (memo[index]!=null) return memo[index]; // base case 2: if we already have the information on this index, return it
-        if (s[index] > 0) { // as long as its bigger than 0, we can at least  use it as a single digit
-            result += recur(index+1); // move forward by 1
+    if (s.length < 0 || s[0] === '0') return 0;
+    let cache = [];
+    const DFS = (index) => {
+        let result = 0;
+        if (index == s.length) {
+            return 1;
         }
-		
-		// as long as 1. we are not starting with 0 etc. `04`
-		//            2. our next digit isn't null
-		//            3. we can form a two digit number thats smaller than 27
+        if (cache[index] != null) {
+            return cache[index];
+        }
+        if (s[index] > 0) {
+          result += DFS(index + 1);  
+        } 
         if (s[index] != 0 && s[index+1] != null && s[index]+s[index+1] < 27) {
-			// lets make this two digit together and move forward by 2
-            result += recur(index+2);
-        }
-        memo[index] = result; // given the current index, store how many different ways we can decode
+          result += DFS(index + 2);  
+        } 
+        cache[index] = result;
         return result;
     }
-    return recur(0);
+    return DFS(0);
 };
