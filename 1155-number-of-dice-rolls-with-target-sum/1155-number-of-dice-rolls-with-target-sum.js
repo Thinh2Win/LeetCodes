1cae17e4e93e9973a0bd76195b1d62fe55ef844e
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-10-21 16:06:54 -0700

    Time: 787 ms (5.05%), Space: 53.8 MB (10.30%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.js
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.js
@@ -4,20 +4,25 @@
  * @param {number} target
  * @return {number}
  */
-var numRollsToTarget = function(n, k, target, memo = {}) {
-        if (n === 0 && target === 0) {
-        return 1;
+var numRollsToTarget = function(n, k, target) {
+    let memo = {};
+    const DFS = (diceRolled, diff) => {
+        if (diff === 0 && diceRolled === 0) {
+            return 1;
+        }
+        if (diceRolled === 0 || diff <= 0) {
+            return 0;
+        }
+         if ( memo[`${diceRolled}-${diff}`] !== undefined) {
+            return  memo[`${diceRolled}-${diff}`];
+        }
+        let result = 0, M = Math.pow(10, 9) + 7;
+        for (let i = 1; i <= k; i++) {
+            result = (result + DFS(diceRolled - 1, diff - i)) % M;
+        }
+        memo[`${diceRolled}-${diff}`] = result;
+        return result;
     }
-    if (n === 0 || target <= 0) {
-        return 0;
-    }
-    if (memo[`${n}-${target}`] !== undefined) {
-        return memo[`${n}-${target}`];
-    }
-    let count = 0, M = Math.pow(10, 9) + 7;
-    for (let i = 1; i <= k; i++) {
-        count = (count + numRollsToTarget(n-1, k, target - i, memo)) % M;
-    }
-    memo[`${n}-${target}`] = count;
-    return count;
+    console.log(memo)
+    return DFS(n, target);
 };
