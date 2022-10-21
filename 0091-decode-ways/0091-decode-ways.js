b9580619d66792b0f0c92077a30f98aa878fa563
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-10-21 12:55:10 -0700

    Time: 202 ms (11.14%), Space: 46.7 MB (12.31%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.js
+++ 0091-decode-ways/0091-decode-ways.js
@@ -3,20 +3,20 @@
  * @return {number}
  */
 var numDecodings = function(s) {
-    if (s.length < 0 || s[0] === '0') return 0;
-    let cache = {};
+    let memo = {};
     const DFS = (index) => {
         let result = 0;
+        if (memo[index] !== undefined) {
+            return memo[index];
+        }
         if (index === s.length) {
             return 1;
         }
-        if (cache[index] !== undefined) {
-            return cache[index];
-        }
-        result += s[index] > 0 ? DFS(index + 1) : 0;
-        result += +s[index] !== 0 && s[index+1] !== undefined && s[index]+s[index+1] < 27 ? DFS(index + 2) : 0;
-        cache[index] = result;
+console.log(+s[index] + +s[index + 1])
+        result += +s[index] > 0 ? DFS(index + 1) : 0;
+        result += +s[index] !== 0 && s[index + 1] !== undefined && +(s[index] + s[index + 1]) <= 26 ? DFS(index + 2) : 0;
+        memo[index] = result;
         return result;
-    }
+    };
     return DFS(0);
 };
