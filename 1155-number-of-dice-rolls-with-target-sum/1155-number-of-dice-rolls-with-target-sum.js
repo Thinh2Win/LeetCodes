2799393a2c4c8e324850bc79376443a79445642f
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-10-21 14:30:50 -0700

    Time: 376 ms (22.73%), Space: 53.5 MB (11.21%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.js
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.js
@@ -4,20 +4,20 @@
  * @param {number} target
  * @return {number}
  */
-var numRollsToTarget = function(n, k, target, memo = new Map()) {
+var numRollsToTarget = function(n, k, target, memo = {}) {
         if (n === 0 && target === 0) {
         return 1;
     }
     if (n === 0 || target <= 0) {
         return 0;
     }
-    if (memo.has(`${n}-${target}`)) {
-        return memo.get(`${n}-${target}`);
+    if (memo[`${n}-${target}`] !== undefined) {
+        return memo[`${n}-${target}`];
     }
     let count = 0, M = Math.pow(10, 9) + 7;
     for (let i = 1; i <= k; i++) {
         count = (count + numRollsToTarget(n-1, k, target - i, memo)) % M;
     }
-    memo.set(`${n}-${target}`, count);
+    memo[`${n}-${target}`] = count;
     return count;
 };
