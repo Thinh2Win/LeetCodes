151816669192a59ae93977fb9ccad339ff513a26
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-10-24 16:27:41 -0700

    Time: 256 ms (5.13%), Space: 50.6 MB (5.08%) - LeetHub

---
+++ 0112-path-sum/0112-path-sum.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, targetSum) {
+    if (!root) return false;
+    
+    let hasTarget = false; 
+    const DFS = (currentNode, currentSum) => {
+        if (!currentNode) {
+            return;
+        }
+        currentSum += currentNode.val;
+        console.log(currentSum)
+        if (!currentNode.left && !currentNode.right && currentSum === targetSum) {
+            return hasTarget = true;
+        }
+        DFS(currentNode.left, currentSum);
+        DFS(currentNode.right, currentSum);
+    };
+    DFS(root, 0);
+    return hasTarget;
+};
