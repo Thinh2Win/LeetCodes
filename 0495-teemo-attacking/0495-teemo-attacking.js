80de77b18a17f7cc69a1a5ba2ea9c8bdbd0b767d
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-10-28 18:26:19 -0700

    Time: 1906 ms (5.17%), Space: 64.5 MB (5.17%) - LeetHub

--- 0495-teemo-attacking/0495-teemo-attacking.js
+++ 0495-teemo-attacking/0495-teemo-attacking.js
@@ -4,27 +4,12 @@
  * @return {number}
  */
 var findPoisonedDuration = function(timeSeries, duration) {
-    if (duration === 0) return 0;
-    let intervals = [];
-    timeSeries.forEach(time => intervals.push([time, time + duration - 1]));
-    let merged = [intervals[0]];
-    intervals.forEach(interval => {
-       if (merged.at(-1)[1] >= interval[0]) {
-           merged[merged.length - 1] = [merged.at(-1)[0], interval[1]];
-       } else {
-           merged.push(interval);
-       } 
-    });
-    let count = 0;
-    merged.forEach(merge => {
-       let diff = merge[1] - merge[0];
-        if (diff > 1) {
-            count += diff + 1
-        } else if (diff === 1) {
-            count += 2; 
-        } else {
-            count += 1;
-        }
-    });
-    return count;
+    if (timeSeries.length === 0 ) return 0; 
+    let res = duration;
+    for (let i = 1; i < timeSeries.length; i++) {
+        console.log(res)
+            res += Math.min(timeSeries[i] - timeSeries[i - 1], duration);
+        console.log(res)
+    }
+    return res; 
 };
