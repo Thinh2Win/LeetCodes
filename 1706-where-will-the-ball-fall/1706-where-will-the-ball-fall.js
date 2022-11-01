4c690bdc1499941f589671862ff42437751ec805
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-11-01 10:21:43 -0700

    Time: 312 ms (5.13%), Space: 52 MB (5.13%) - LeetHub

---
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.js
@@ -0,0 +1,64 @@
+/**
+ * @param {number[][]} grid
+ * @return {number[]}
+ */
+const isTrapped = (row, col, grid) => {
+    let rowMax = grid.length - 1;
+    let colMax = grid[0].length - 1;
+    if (grid[row][col] === 1) {
+        if (col + 1 > colMax) {
+            return true;
+        }
+        if (grid[row][col + 1] === -1) {
+            return true;
+        }
+    }
+    if (grid[row][col] === -1) {
+        if (col - 1 < 0) {
+            return true;
+        }
+        if (grid[row][col - 1] === 1) {
+            return true;
+        }
+    }
+    return false;
+}
+
+var findBall = function(grid) {
+    let dp = new Array(grid.length).fill(0).map(zero => new Array(grid[0].length).fill(0));
+    let answer = new Array(grid[0].length).fill(-1);
+    const DFS = (row, col, origin) => {
+    if (isTrapped(row, col, grid)) {
+        dp[row][col] = false;
+        return false;
+    }
+    if (row === grid.length - 1 && !isTrapped(row, col, grid)) {
+        if (grid[row][col] === -1) {
+            answer[origin] = col - 1;
+            dp[row][col - 1] = true;
+        } else {
+            answer[origin] = col + 1;
+            dp[row][col + 1] = true;
+        }
+        return true; 
+    }
+    if (dp[row][col] === true || dp[row][col] === false) {
+        return dp[row][col];
+    }
+    let result = null;
+    if (grid[row][col] === -1) {
+        result = DFS(row + 1, col - 1, origin);
+    } else if (grid[row][col] === 1){
+        result = DFS(row + 1, col + 1, origin);
+    }
+    dp[row][col] = result;
+    return result;
+    }
+    for (let row = 0; row < 1; row++) {
+        for (let col = 0; col < grid[0].length; col++) {
+            DFS(row, col, col)
+        }
+    }
+    console.log(dp)
+    return answer;
+};
