0776e79b44fb82e2fc8267d07a5558c19fd1eb56
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-11-04 10:46:45 -0700

    Time: 104 ms (22.15%), Space: 41.3 MB (97.03%) - LeetHub

---
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.js
@@ -0,0 +1,38 @@
+/**
+ * @param {string} start
+ * @param {string} end
+ * @param {string[]} bank
+ * @return {number}
+ */
+
+const isOneOff = (gene1, gene2) => {
+    let diff = 0;
+    for (let i = 0; i < gene1.length; i++) {
+        if (diff > 1) {
+            return false;
+        }
+        if (gene1[i] !== gene2[i]) {
+            diff += 1;
+        }
+    }
+    return diff > 1 ? false : true;
+}
+
+var minMutation = function(start, end, bank) {
+    let min = Infinity;
+    if (!bank.includes(end)) return -1;
+    const DFS = (currentGene, remaining, lvl) => {
+    if (isOneOff(currentGene, start)) {
+        min = Math.min(min, lvl);
+        return;
+    }
+
+    for (let i = 0; i < remaining.length; i++) {
+        if (isOneOff(currentGene, remaining[i])) {
+            DFS(remaining[i], remaining.filter(gene => gene !== remaining[i]), lvl + 1);
+        }
+    }
+}
+    DFS(end, bank.filter(gene => gene !== end), 1);
+    return min === Infinity ? -1 : min;
+};
