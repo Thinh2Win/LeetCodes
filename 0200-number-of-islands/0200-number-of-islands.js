4dcbbdae3148c50ada5216c131747ada7c5ad2fa
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-11-07 12:51:59 -0800

    Time: 144 ms (49.71%), Space: 48 MB (43.12%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -0,0 +1,45 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function(grid) {
+    // iterate through grid 
+    // if we find a 1 we do BFS and change all attatched 1's to 0's 
+    // increment counter 
+    // return counter 
+    let counter = 0;
+    const BFS = (coordinates) => {
+    let q = [coordinates];
+    grid[coordinates[0]][coordinates[1]] = 0;
+    while (q.length) {
+        let [row, col] = q.shift();
+        if (row - 1 >= 0 && grid[row - 1][col] === '1') {
+            grid[row - 1][col] = 0;
+            q.push([row - 1, col]);
+        }
+        if (row + 1 < grid.length && grid[row + 1][col] === '1') {
+            grid[row + 1][col] = 0;
+            q.push([row + 1, col]);
+        }
+        if (col - 1 >= 0 && grid[row][col - 1] === '1') {
+            grid[row][col - 1] = 0;
+            q.push([row, col - 1]);
+        }
+        if (col + 1 >= 0 && grid[row][col + 1] === '1') {
+            grid[row][col + 1] = 0;
+            q.push([row, col + 1]);
+        }
+    }
+}
+
+    for (let row = 0; row < grid.length; row++) {
+        for (let col = 0; col < grid[row].length; col++) {
+            if (grid[row][col] === '1') {
+                counter += 1;
+                BFS([row, col]);
+            }
+        }
+    }
+    return counter;
+};
+
