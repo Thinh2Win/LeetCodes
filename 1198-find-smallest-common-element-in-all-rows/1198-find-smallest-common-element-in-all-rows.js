36d76ebb51bec536c73ab7c83324374a82ce9801
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-11-10 13:24:01 -0800

    Time: 339 ms (5.39%), Space: 88 MB (5.88%) - LeetHub

--- 1198-find-smallest-common-element-in-all-rows/1198-find-smallest-common-element-in-all-rows.js
+++ 1198-find-smallest-common-element-in-all-rows/1198-find-smallest-common-element-in-all-rows.js
@@ -4,13 +4,18 @@
  */
 var smallestCommonElement = function(mat) {
     let maps = [];
+    let max = [-Infinity, null];
     for (let row = 0; row < mat.length; row++) {
         let map = {};
         for (let i = 0; i < mat[row].length; i++) {
             map[mat[row][i]] = true;
+            if (i === 0 && mat[row][i] > max[0]) {
+                max = [mat[row][i], row];
+            }
         }
         maps.push(map);
     }
+    console.log(max)
     for (let col = 0; col < mat[0].length; col++) {
         let currentNum = mat[0][col];
         for (var row = 0; row < mat.length; row++) {
