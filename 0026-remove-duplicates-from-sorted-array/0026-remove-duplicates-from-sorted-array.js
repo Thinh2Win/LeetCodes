f560ed2ba020081b309d0c3eafa8bb9ccda2a3a6
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-11-11 11:02:11 -0800

    Time: 259 ms (8.35%), Space: 50.3 MB (5.27%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let L = 1;
+    while (nums[L] !== nums[L - 1]) {
+        L += 1;
+    }
+    let currentNum = nums[L];
+    for (let R = L + 1; R < nums.length; R++) {
+        if (currentNum !== nums[R]) {
+            [nums[L], nums[R]] = [nums[R], nums[L]];
+            currentNum = nums[L];
+            console.log(currentNum)
+            L += 1;
+        }
+    }
+    return L;
+};
