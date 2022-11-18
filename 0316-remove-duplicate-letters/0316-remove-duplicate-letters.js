428be95b87177dc9c8be879a8c9917e9859b4539
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-11-18 13:23:13 -0800

    Time: 148 ms (11.23%), Space: 43.3 MB (55.61%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeDuplicateLetters = function(s) {
+    let map = {};
+    for (let i = 0; i < s.length; i++) {
+        let letter = s[i];
+        if (map[letter] === undefined) {
+            map[letter] = 1;
+        } else {
+            map[letter] += 1;
+        }
+    }
+    let stack = [s[0]];
+    for (let j = 1; j < s.length; j++) {
+        let current = s[j];
+        while (stack.at(-1) > current && map[stack.at(-1)] > 1 && !stack.includes(current)) {
+            let removed = stack.pop();
+            map[removed] -= 1;
+        }
+        if (!stack.includes(current)) {
+            stack.push(current);
+        } else {
+            map[current] -= 1;
+        }
+    }
+    return stack.join('');
+};
