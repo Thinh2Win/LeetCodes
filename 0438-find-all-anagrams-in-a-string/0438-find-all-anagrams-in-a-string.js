44ba1b5313c2fc0c77e37219e17da215df9a5500
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-11-25 09:03:21 -0800

    Time: 329 ms (27.71%), Space: 48.8 MB (44.57%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {number[]}
+ */
+const isAnagram = (map1, map2) => {
+    for (key in map2) {
+        if (map2[key] !== map1[key]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+var findAnagrams = function(s, p) {
+    let map1 = {};
+    let map2 = {};
+    let answer = [];
+    let L = 0;
+    p.split('').forEach(letter => map2[letter] ? map2[letter] += 1 : map2[letter] = 1);
+    for (let R = 0; R < s.length; R++) {
+        if (R - L >= p.length) {
+            map1[s[L]] -= 1; 
+            L += 1;
+        }
+        map1[s[R]] ? map1[s[R]] += 1 : map1[s[R]] = 1; 
+        if (R >= p.length - 1 && isAnagram(map1, map2)) {
+            answer.push(L);
+        }
+    }
+    return answer; 
+};
