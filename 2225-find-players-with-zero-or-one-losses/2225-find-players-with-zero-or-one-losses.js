e52c90689f76980fd07ef6edb1bb2f58c41d1d11
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-11-28 13:02:32 -0800

    Time: 856 ms (24.12%), Space: 116.9 MB (50.35%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.js
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.js
@@ -4,21 +4,20 @@
  */
 var findWinners = function(matches) {
     let winners = new Set();
+    let losers = new Set();
     let losses = {};
     matches.forEach(match => {
-        let winner = match[0];
+       let winner = match[0];
         let loser = match[1];
         losses[loser] ? losses[loser] += 1 : losses[loser] = 1;
-        winners.add(winner)
-    });
-    let losers = [];
-    Object.keys(losses).forEach(loser => {
-        winners.delete(+loser);
-        if (losses[loser] === 1) {
-            losers.push(JSON.parse(loser));
+        if (!losses[winner]) {
+            winners.add(winner);    
         }
+        losers.add(loser);
+        if (losses[loser] > 1) {
+            losers.delete(+loser)
+        }
+        winners.delete(+loser);
     });
-    return [[...winners].sort((a,b) => a - b),losers.sort((a,b) => a - b)];
-    
-    // iterate through our map, if player has lost 1 time, we push into loser     // array 
+    return [[...winners].sort((a,b) => a - b), [...losers].sort((a,b) => a - b)]
 };
