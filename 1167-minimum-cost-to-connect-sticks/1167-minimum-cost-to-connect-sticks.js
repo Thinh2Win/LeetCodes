485e52a5b6fc4b4682d12c7a752c4c52c78e8d92
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-12-23 12:36:44 -0800

    Time: 176 ms (74.14%), Space: 48.9 MB (68.97%) - LeetHub

--- 1167-minimum-cost-to-connect-sticks/1167-minimum-cost-to-connect-sticks.js
+++ 1167-minimum-cost-to-connect-sticks/1167-minimum-cost-to-connect-sticks.js
@@ -3,23 +3,24 @@
  * @return {number}
  */
 var connectSticks = function(sticks) {
-    if (sticks.length <= 1) return 0;
-    sticks.sort((a, b) => a - b);
+    if (sticks.length <= 1) return 0
+    sticks.sort((a, b) => b - a);
     let total = 0;
     while (sticks.length) {
-        const combined = sticks.shift() + sticks.shift();
+        let combined = sticks.pop() + sticks.pop();
         total += combined;
         if (sticks.length === 0) return total;
+
+        // re-insert combined stick back into array
         let L = 0;
         let R = sticks.length;
         while (L < R) {
-            const mp = L + Math.floor((R - L) / 2); 
+            let mp = L + Math.floor((R - L) / 2);
             if (combined < sticks[mp]) {
-                R = mp;
-            } else {
                 L = mp + 1;
+            } else {
+                R = mp;
             }
-
         }
         sticks.splice(L, 0, combined);
     }
