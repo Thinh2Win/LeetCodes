bfcc0964b71d071df10736efb3ceb379fcb96b64
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-12-26 16:21:15 -0800

    Time: 81 ms (87.41%), Space: 47.3 MB (30.88%) - LeetHub

--- 0055-jump-game/0055-jump-game.js
+++ 0055-jump-game/0055-jump-game.js
@@ -2,18 +2,15 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var canJump = function(array) {
-    let dp = new Array(array.length - 1);
-    for (let i = array.length - 2; i >= 0; i--) {
-        if (array[i] + i >= array.length - 1) {
-            dp[i] = 1;
-        } else {
-            let min = Infinity;
-            for (let j = i + 1; j <= i + array[i]; j++) {
-                min = Math.min(min, dp[j]);
-            }
-            dp[i] = min + 1;
-        }
+var canJump = function(nums) {
+    let dp = new Array(nums.length).fill(false);
+    dp[dp.length - 1] = true;
+    let recTrue = dp.length - 1;
+    for (let i = dp.length - 2; i >= 0; i--) {
+        if (recTrue - i <= nums[i]) {
+            dp[i] = true; 
+            recTrue = i;
+        } 
     }
-    return dp[0] === Infinity ? false : true;
+    return dp[0];
 };
