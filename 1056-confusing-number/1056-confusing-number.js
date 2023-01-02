a22680d4714561a229044d8b9c11cd2b38726d0b
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-01-02 12:10:37 -0800

    Time: 64 ms (61.19%), Space: 42.6 MB (8.96%) - LeetHub

---
+++ 1056-confusing-number/1056-confusing-number.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+let invalids = {
+    '2': true,
+    '3': true,
+    '4': true,
+    '5': true,
+    '7': true,
+}
+var confusingNumber = function(n) {
+    let rotate = [];
+    n = `${n}`;
+    for (let j = 0; j < n.length; j++) {
+        if (invalids[n[j]]) {
+            return false;
+        }
+        if (n[j] === '6') {
+            rotate.unshift('9');  
+        } else if (n[j] === '9') {
+            rotate.unshift('6');   
+        } else {
+            rotate.unshift(n[j]);
+        }
+    }
+    console.log(n, rotate)
+    if (n === rotate.join('')) return false;
+    return true;
+};
