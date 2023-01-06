4eef1beb4f99b0e5c4081153811730e410851b6b
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-01-06 09:13:56 -0800

    Time: 203 ms (51.26%), Space: 53.2 MB (59.09%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -4,18 +4,13 @@
  */
 var groupAnagrams = function(strs) {
     let map = {};
-    let set = new Set();
-    let copy = strs.slice();
-    for (let i = 0; i < copy.length; i++) {
-        copy[i] = copy[i].split('').sort().join('');
-        set.add(copy[i])
-    }
-    [...set].forEach((word, idx) => map[word] = idx);
-    let answer = new Array(Object.keys(map).length).fill(0).map(zero => []);
-    copy.forEach((string, idx) => {
-        if (map[string] !== undefined) {
-            answer[map[string]].push(strs[idx]);
-        }
+    strs.forEach((word, idx) => {
+        map[idx] = word;
+        strs[idx] = word.split('').sort().join('');
     });
-    return answer;
+    let anagrams = {};
+    strs.forEach((word, idx) => {
+       anagrams[word] ? anagrams[word].push(map[idx]) : anagrams[word] = [map[idx]]; 
+    });
+    return Object.values(anagrams);
 };
