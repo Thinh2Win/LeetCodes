726d30563fa1af9d1043f581d3012c76fe8fec38
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-01-09 13:29:39 -0800

    Time: 115 ms (99.60%), Space: 53 MB (86.75%) - LeetHub

---
+++ 0721-accounts-merge/0721-accounts-merge.js
@@ -0,0 +1,47 @@
+/**
+ * @param {string[][]} accounts
+ * @return {string[][]}
+ */
+var accountsMerge = function(accounts) {
+    let map = {};
+    let adjList = new Array(accounts.length).fill(0).map(zero => []);
+    let seen = {};
+    let answer = [];
+    accounts.forEach((account, idx) => {
+        for (let i = 1; i < account.length; i++) {
+            let email = account[i];
+             if (map[email] === undefined) {
+                map[email] = idx;
+            } else {
+                let connectedIdx = map[email];
+                adjList[connectedIdx].push(idx);
+                adjList[idx].push(connectedIdx);
+            }
+        }
+    });
+    const DFS = (idx, set) => {
+        if (seen[idx]) {
+            return;
+        }
+        accounts[idx].forEach(val => set.add(val));
+        seen[idx] = true;
+        for (let k = 0; k < adjList[idx].length; k++) {
+            if (seen[adjList[idx][k]]) continue;
+            DFS(adjList[idx][k], set);
+        }
+        return set;
+    }
+    for (let j = 0; j < adjList.length; j++) {
+        if (seen[j]) continue;
+        if (adjList[j].length === 0) {
+            answer.push(accounts[j].sort());
+        } else {
+            let mergedAccount = [...DFS(j, new Set())];
+            let name = mergedAccount.shift();
+            mergedAccount.sort();
+            mergedAccount.unshift(name);
+            answer.push(mergedAccount);
+        }
+    }
+    return answer;
+};
