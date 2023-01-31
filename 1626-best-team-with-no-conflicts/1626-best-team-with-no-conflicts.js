d71d8cef827f053345df74fdad623d107e7a88d6
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-01-31 14:38:56 -0800

    Time: 175 ms (33.33%), Space: 45.8 MB (77.78%) - LeetHub

---
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} scores
+ * @param {number[]} ages
+ * @return {number}
+ */
+var bestTeamScore = function(scores, ages) {
+      const n = ages.length;
+  const ageScorePair = new Array(n);
+  for (let i = 0; i < n; i++) {
+    ageScorePair[i] = [ages[i], scores[i]];
+  }
+  ageScorePair.sort((a, b) => a[0] === b[0] ? a[1] - b[1] : a[0] - b[0]);
+  const dp = new Array(n).fill(0);
+  let maxScore = 0;
+  for (let i = 0; i < n; i++) {
+    dp[i] = ageScorePair[i][1];
+    for (let j = 0; j < i; j++) {
+      if (ageScorePair[i][1] >= ageScorePair[j][1]) {
+        dp[i] = Math.max(dp[i], dp[j] + ageScorePair[i][1]);
+      }
+    }
+    maxScore = Math.max(maxScore, dp[i]);
+  }
+  return maxScore;
+};
