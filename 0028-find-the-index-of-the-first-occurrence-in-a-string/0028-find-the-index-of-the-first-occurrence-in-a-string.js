64fdca6a9bff983682ca109812d493622b1af220
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-02-01 15:27:55 -0800

    Time: 67 ms (62.37%), Space: 43 MB (11.43%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -7,11 +7,11 @@ var strStr = function(haystack, needle) {
     for (let i = 0; i < haystack.length; i++) {
         let j = i;
         let k = 0;
-        while (haystack[j] === needle[k] && k < needle.length) {
+        while (haystack[j] && haystack[j] === needle[k]) {
             j += 1;
             k += 1;
-            if (k === needle.length) return i;
         }
+        if (j - i === needle.length) return i;
     }
     return -1;
 };
