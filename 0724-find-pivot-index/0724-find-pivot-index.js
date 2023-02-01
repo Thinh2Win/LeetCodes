51ec0479a09c98d982d2f084fc4d41c5baadca15
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-02-01 10:05:36 -0800

    Time: 78 ms (79.86%), Space: 43.8 MB (96.98%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function(nums) {
+    let answer = -1;
+    let total = nums.reduce((a, b) => a + b, 0);
+    let L = 0;
+    let R = total;
+    for (let i = 0; i < nums.length; i++) {
+        R -= nums[i];
+        if (L === R) {
+            return i;
+        }
+        L += nums[i];
+    }
+    return answer;
+};
