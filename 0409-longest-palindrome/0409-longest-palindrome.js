961a0e0df46866a5784fa588b658926a622e40c1
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-02-06 11:21:34 -0800

    Time: 75 ms (55.56%), Space: 44.6 MB (18.51%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestPalindrome = function(s) {
+    let length = 0;
+    let set = new Set();
+    for (let i = 0; i < s.length; i++) {
+        if (set.has(s[i])) {
+            length += 2;
+            set.delete(s[i]);
+        } else {
+            set.add(s[i]);
+        }
+    }
+    return set.size > 0 ? length + 1 : length; 
+};
