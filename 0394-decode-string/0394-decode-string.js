ba716936c801194490cb72df0d3f302b11007735
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-02-15 17:49:25 -0800

    Time: 59 ms (81.87%), Space: 42.1 MB (30.70%) - LeetHub

--- 0394-decode-string/0394-decode-string.js
+++ 0394-decode-string/0394-decode-string.js
@@ -20,11 +20,6 @@ var decodeString = function(s) {
             let string = new Array(+num).fill(letters).join('');
             if (stack.length && stack[stack.length - 1] !== '[') {
                 stack[stack.length - 1] = stack.at(-1) + string;
-                while (stack.length >= 2 && isNaN(stack.at(-2)) && stack.at(-2) !== '[') {
-                    let s1 = stack.pop();
-                    let s2 = stack.pop();
-                    stack.push(s2 + s1);
-                }
             } else {
                 stack.push(string);
             }
