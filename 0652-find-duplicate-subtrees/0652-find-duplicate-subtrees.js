cb60930d203ea10c7b0d405ae0599f339cc4151b
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-02-28 13:17:37 -0800

    Time: 107 ms (72.03%), Space: 56.3 MB (42.37%) - LeetHub

---
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode[]}
+ */
+var findDuplicateSubtrees = function(root) {
+    let map = {};
+    let answer = [];
+    const DFS = (node) => {
+        if (!node) return '*';
+        let subtree = `${node.val},${DFS(node.left)},${DFS(node.right)}`;
+        map[subtree] = (map[subtree] || 0) + 1;
+        if (map[subtree] === 2) {
+            answer.push(node);
+        }
+        return subtree;
+    };
+    DFS(root);
+    return answer;
+};
