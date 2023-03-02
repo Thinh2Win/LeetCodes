6bba72b23adb7ba2310b004e60a6d397f09b6eaf
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-03-02 09:10:38 -0800

    Time: 80 ms (41.52%), Space: 44.8 MB (21.80%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.js
@@ -0,0 +1,25 @@
+/**
+ * @param {character[]} chars
+ * @return {number}
+ */
+var compress = function(chars) {
+    let start = chars[0];
+    while (start === chars[1]) {
+        chars[0] += chars[1];
+        chars.splice(1, 1);
+    };
+    if (chars[0].length > 1) chars[0] = `${chars[0][0]}${chars[0].length}`;
+    while (chars.length > 1) {
+        let char = chars[1];
+        while (char === chars[2]) {
+            chars[1] += chars[2];
+            chars.splice(2,1);
+        }
+        chars[0] += chars[1].length > 1 ? `${chars[1][0]}${chars[1].length}` : chars[1];
+        chars.splice(1,1);
+    }
+    for (let j = 0; j < chars[0].length; j++) {
+        chars.push(chars[0][j]);
+    }
+    chars.shift();
+};
