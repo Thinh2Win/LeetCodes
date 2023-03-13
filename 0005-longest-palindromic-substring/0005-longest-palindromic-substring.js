8b733457e4952448b9fb23fadc6e1f7a7389e7f9
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-03-13 11:05:54 -0700

    Time: 375 ms (36.82%), Space: 48.4 MB (35.78%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -3,19 +3,21 @@
  * @return {string}
  */
 var longestPalindrome = function(s) {
-    let string = '-' + s.split('').join('-') + '-';
-    let answer = '';
-    for (let i = 0; i < string.length; i++) {
+    let longest = [0, ''];
+    let string = `*${s.split('').join('*')}*`;
+    for (let i = 1; i < string.length; i++) {
         let L = i - 1;
         let R = i + 1;
-        while (L >= 0 && string[L] === string[R]) {
+        let count = 1;
+        while (string[L] === string[R] && L >= 0) {
+            count += 2;
             L -= 1;
             R += 1;
-        }
-        let substring = string.slice(L + 1, R);
-        if (substring.length > answer.length) {
-            answer = substring;
-        }
+        };
+        if (Math.floor(count / 2) > longest[0]) {
+            longest[0] = Math.floor(count / 2);
+          longest[1] = string.split('').slice(L + 1, R - 1).join('').split('*').join('');  
+        } 
     }
-    return answer.split('-').join('');
+    return longest[1];
 };
