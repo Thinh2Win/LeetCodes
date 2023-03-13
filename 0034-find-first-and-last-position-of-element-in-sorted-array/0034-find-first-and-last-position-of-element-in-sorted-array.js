92fb33d04ac23d323b140608b12d06eb2db2ee0a
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-03-13 16:13:01 -0700

    Time: 62 ms (69.60%), Space: 43.1 MB (11.02%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -4,31 +4,29 @@
  * @return {number[]}
  */
 var searchRange = function(nums, target) {
-    if (nums.length === 0) return [-1,-1];
+    let answer = [-1, -1];
     let L = 0;
     let R = nums.length - 1;
     while (L < R) {
         let mp = Math.floor((R - L) / 2) + L;
-        if (nums[mp] > target) {
-            R = mp - 1;
-        } else if (nums[mp] < target) {
+        if (nums[mp] >= target) {
+            R = mp;
+        } else {
+            L = mp + 1;
+        }
+    }
+    if (nums[L] !== target) return answer;
+    answer[0] = L;
+
+    R = nums.length - 1;
+    while (L < R) {
+        let mp = Math.floor((R - L) / 2) + L;
+        if (nums[mp] <= target) {
             L = mp + 1;
         } else {
-            // mp === target
-            L = mp;
             R = mp;
         }
     }
-    if (nums[L] !== target) return [-1,-1];
-    let i1 = L;
-    let i2 = R;
-    while (nums[i1] === nums[L] || nums[i2] === nums[L]) {
-        if (nums[i1] === nums[L]) {
-            i1 -= 1;
-        }
-        if (nums[i2] === nums[L]) {
-            i2 += 1;
-        }
-    }
-    return [i1 + 1, i2 - 1];
+    answer[1] = nums[L] === target ? L : L - 1;
+    return answer;
 };
