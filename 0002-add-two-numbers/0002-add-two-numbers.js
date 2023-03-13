315a761eeefd8cadea83575f0e3aa5ef7deedfaf
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-03-13 10:26:52 -0700

    Time: 94 ms (92.35%), Space: 48.1 MB (15.76%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.js
+++ 0002-add-two-numbers/0002-add-two-numbers.js
@@ -11,31 +11,32 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-    let total = [];
-    let idx = 0;
-    let node1 = l1;
-    let node2 = l2;
-    while (node1 !== null || node2 !== null) {
-        if (total[idx] === undefined) total[idx] = 0;
-        let num1 = node1 === null ? 0 : +node1.val;
-        let num2 = node2 === null ? 0 : +node2.val;
-        let sum = num1 + num2 + total[idx];
-        if (sum > 9) {
-            let [d1, d2] = `${sum}`.split('');
-            total[idx] = +d2;
-            total[idx + 1] = +d1;
-        } else {
-            total[idx] = sum;
-        }
-        node1 = node1 && node1.next;
-        node2 = node2 && node2.next;
-        idx += 1;
+    let head;
+    let carry = 0;
+    if (l1.val + l2.val > 9) {
+        carry = 1;
+        head = new ListNode(+(`${l1.val + l2.val}`[1]));
+    } else {
+        head = new ListNode(l1.val + l2.val);
     }
-    let head = new ListNode(`${total[0]}`);
     let curr = head;
-    for (let i = 1; i < total.length; i++) {
-        curr.next = new ListNode(`${total[i]}`);
+    let n1 = l1.next;
+    let n2 = l2.next; 
+    while (n1 || n2) {
+        let sum = (n1?.val || 0) + (n2?.val || 0) + carry;
+        if (sum > 9) {
+            curr.next = new ListNode(+(`${sum}`[1]));
+            carry = 1;
+        } else {
+            curr.next = new ListNode(sum);
+            carry = 0;
+        }
         curr = curr.next;
+        if (n1) n1 = n1.next;
+        if (n2) n2 = n2.next;
+    }
+    if (carry === 1) {
+        curr.next = new ListNode(1);
     }
     return head;
 };
