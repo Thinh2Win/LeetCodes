f725afe70173a3082d5c53e7efbc24d8048d1e33
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-03-13 12:08:28 -0700

    Time: 213 ms (34.12%), Space: 54.3 MB (99.84%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -3,40 +3,28 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    // 3 pointers 
-    // i, L, and R
-    let answer = [];
-    // sort nums array by acending order
+    // keep track of 3 sums
+    let answer = new Set();
+    // sort the array 
     nums.sort((a, b) => a - b);
-    // keep track of the sum of 3 pointers 
+    // iterate through nums array 
     for (let i = 0; i < nums.length; i++) {
+        // create our window variables L R  
         if (nums[i] === nums[i - 1]) continue;
         let L = i + 1;
         let R = nums.length - 1;
         while (L < R) {
-            let total = nums[L] + nums[R] + nums[i];
-            if (total > 0) {
+            let sum = nums[i] + nums[L] + nums[R];
+            if (sum > 0) {
                 R -= 1;
-            } else if (total < 0) {
+            } else if (sum < 0) {
                 L += 1;
             } else {
-                answer.push([nums[i], nums[L], nums[R]]);
-                L += 1;
+                answer.add(JSON.stringify([nums[i], nums[L], nums[R]]));
                 R -= 1;
-                while (nums[L] === nums[L - 1]) {
-                    L += 1;
-                }
-                while (nums[R] === nums[R + 1]) {
-                    R -= 1;
-                }
+                L += 1;
             }
         }
     }
-    return answer;
-    // if the sum is greater than target '0'
-    // we know to decrement R to shrink sum
-    // if sum is less than target 
-    // we know to increment L to increase sum 
-    // if sum is equal to target, put i L and R into an array 
-    // and push into answer array 
+    return [...answer].map(set => JSON.parse(set));
 };
