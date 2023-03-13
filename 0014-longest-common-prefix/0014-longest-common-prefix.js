835fc5f3783d7c5c798889a219efe02d318b3cb0
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-03-13 11:49:23 -0700

    Time: 64 ms (74.99%), Space: 42.5 MB (52.14%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -3,19 +3,17 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    let answer = '';
     let longest = '';
-    strs.forEach((string, idx) => {
-        if (string.length > longest.length) {
-            longest = string;
-        };
+    let shortest = [Infinity, ''];
+    strs.forEach(str => {
+       if (str.length < shortest[0]) shortest[1] = str;
     });
-    for (let i = 0; i < longest.length; i++) {
-        if (strs.every(s => s[i] === longest[i])) {
-            answer += longest[i];
+    for (let i = 0; i < shortest[1].length; i++) {
+        if (strs.every(str => str[i] === shortest[1][i])) {
+            longest += shortest[1][i];
         } else {
             break;
         }
     }
-    return answer;
+    return longest;
 };
