31bf0c3006c66f1c0c833b40705b81d0a142e4a5
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-03-13 13:02:50 -0700

    Time: 66 ms (82.38%), Space: 43.8 MB (87.02%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -11,42 +11,37 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
+    if(!list1 && list2) return list2;
+    if(list1 && !list2) return list1;
     if (!list1 && !list2) return null;
-    if (!list1 && list2) return list2;
-    if (list1 && !list2) return list1;
-    let n1 = list1; 
-    let n2 = list2; 
+    let n1 = list1;
+    let n2 = list2;
     let head;
-    if (n1.val > n2.val) {
-        head = new ListNode(n2.val);
-        n2 = n2.next;
-    } else {
-        head = new ListNode(n1.val);
+    if (n1.val < n2.val) {
+        head = n1;
         n1 = n1.next;
+    } else {
+        head = n2;
+        n2 = n2.next;
     }
-    let link = head;
+    let curr = head;
     while (n1 || n2) {
-        if (n1 && n2) {
-            if (n1.val > n2.val) {
-                link.next = new ListNode(n2.val);
-                link = link.next;
-                n2 = n2.next;
-            } else {
-                link.next = new ListNode(n1.val);
-                link = link.next;
-                n1 = n1.next;
-            }
-        } else if (n1 && !n2) {
-            while (n1) {
-                link.next = new ListNode(n1.val);
-                link = link.next;
-                n1 = n1.next;
-            }
-        } else {
-            link.next = new ListNode(n2.val);
-            link = link.next;
+        if (n1 && !n2) {
+            curr.next = n1;
+            n1 = n1.next;
+        } else if (!n1 && n2) {
+            curr.next = n2;
             n2 = n2.next;
+        } else {
+            if (n1.val < n2.val) {
+                curr.next = n1;
+                n1 = n1.next;
+            } else {
+                curr.next = n2;
+                n2 = n2.next;
+            }
         }
+        curr = curr.next;
     }
     return head;
 };
