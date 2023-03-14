7b231cd4810a411ae90e88da9fd61e0fb5767385
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-03-14 16:55:50 -0700

    Time: 62 ms (67.73%), Space: 45.3 MB (6.45%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.js
+++ 0048-rotate-image/0048-rotate-image.js
@@ -3,26 +3,28 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var rotate = function(matrix) {
-    let L = 0;
-    let R = matrix[0].length - 1;
-    while (L < R) {
-        let tL = [L, L];
-        let tR = [L, R];
-        let bR = [R, R];
-        let bL = [R, L];
-        for (let i = L; i < R; i++) {
-            let temp = matrix[tL[0]][tL[1]];
-            matrix[tL[0]][tL[1]] = matrix[bL[0]][bL[1]];
-            matrix[bL[0]][bL[1]] = matrix[bR[0]][bR[1]];
-            matrix[bR[0]][bR[1]] = matrix[tR[0]][tR[1]];
-            matrix[tR[0]][tR[1]] = temp;
+    let rowStart = 0;
+    let rowEnd = matrix.length - 1;
+    let colStart = 0;
+    let colEnd = matrix[0].length - 1;
+    while (rowStart < rowEnd) {
+        let tL = [rowStart, colStart];
+        let tR = [rowStart, colEnd];
+        let bR = [rowEnd, colEnd];
+        let bL = [rowEnd, colStart];
+        for (let i = colStart; i < colEnd; i++) {
+            [matrix[tL[0]][tL[1]], matrix[bL[0]][bL[1]]] = [matrix[bL[0]][bL[1]], matrix[tL[0]][tL[1]]];
+            [matrix[bL[0]][bL[1]], matrix[bR[0]][bR[1]]] = [matrix[bR[0]][bR[1]], matrix[bL[0]][bL[1]]];
+            [matrix[bR[0]][bR[1]], matrix[tR[0]][tR[1]]] = [matrix[tR[0]][tR[1]], matrix[bR[0]][bR[1]]];
             tL[1] += 1;
             tR[0] += 1;
             bR[1] -= 1;
             bL[0] -= 1;
         }
-        L += 1;
-        R -= 1;
+        rowStart += 1;
+        rowEnd -= 1;
+        colStart += 1;
+        colEnd -= 1;
     }
     return matrix;
 };
