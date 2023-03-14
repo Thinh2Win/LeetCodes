fae282abecade6c731e9120d5d1426c19388dd63
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-03-13 17:37:57 -0700

    Time: 70 ms (90.52%), Space: 48.5 MB (58.33%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.js
+++ 0041-first-missing-positive/0041-first-missing-positive.js
@@ -3,22 +3,17 @@
  * @return {number}
  */
 var firstMissingPositive = function(nums) {
-    for (let i = 0; i < nums.length; i++) {
-        let limit = nums.length;
-        if (nums[i] <= limit && nums[i] > 0 && nums[i] !== i + 1) {
-            let temp = nums[nums[i] - 1];
-            nums[nums[i] - 1] = nums[i];
-            while (temp <= limit && temp > 0 && nums[temp - 1] !== temp) {
-                let temp2 = nums[temp - 1];
-                nums[temp - 1] = temp;
-                temp = temp2;
-            };
-        };
-    };
-    for (let j = 0; j < nums.length; j++) {
-        if (nums[j] !== j + 1) {
-            return j + 1;
+    let i = 0;
+    while (i < nums.length) {
+        if (nums[i] <= nums.length && nums[i] > 0 && nums[nums[i] - 1] !== nums[i]) {
+            [nums[nums[i] - 1], nums[i]] = [nums[i], nums[nums[i] - 1]];
+        } else {
+            i += 1;
         }
     }
-    return nums.at(-1) + 1;
+         
+    for (var j = 0; j < nums.length; j++) {
+        if (nums[j] !== j + 1) return j + 1;
+    };
+    return j + 1;
 };
