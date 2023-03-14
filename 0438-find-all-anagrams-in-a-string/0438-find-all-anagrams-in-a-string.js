cf66d743f56e3e3efe9c1480ac46300879b3f76b
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-03-14 13:10:14 -0700

    Time: 172 ms (40.99%), Space: 49.2 MB (39.16%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.js
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.js
@@ -3,28 +3,29 @@
  * @param {string} p
  * @return {number[]}
  */
-
-const isAnagram = (m1, m2) => {
-    for (key in m2) {
-        if (m1[key] !== m2[key]) {
-            return false;
-        }
-    }
-    return true;
-}
 var findAnagrams = function(s, p) {
-    let map1 = {};
-    let map2 = {};
+    // create a map for letters in p
+    // create another map for our window to keep track of letters in window
+    // window will be length of p
+    // as we iterate through s, we take note of all letters in window 
+    // if all letters match, we push current index into answer array 
     let answer = [];
-    p.split('').forEach(letter => map2[letter] ? map2[letter] += 1 : map2[letter] = 1);
+    let pMap = {};
+    p.split('').forEach(letter => {
+       pMap[letter] ? pMap[letter] += 1 : pMap[letter] = 1; 
+    });
+    let wMap = {};
     let L = 0;
     for (let R = 0; R < s.length; R++) {
-        if (R - L >= p.length) {
-            map1[s[L]] -= 1;
+        let wLength = R - L + 1;
+        if (wLength > p.length) {
+            wMap[s[L]] -= 1;
             L += 1;
         }
-        map1[s[R]] ? map1[s[R]] += 1 : map1[s[R]] = 1;
-        if (R - L + 1 === p.length && isAnagram(map1, map2)) {
+        wMap[s[R]] !== undefined ? wMap[s[R]] += 1 : wMap[s[R]] = 1;
+        if (Object.keys(pMap).every(key => {
+        return pMap[key] === wMap[key]
+    })) {
             answer.push(L);
         }
     }
