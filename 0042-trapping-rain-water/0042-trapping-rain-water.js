2d7a64430bb3614ae1ce1017ccad6f6aaedd2087
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-03-14 12:07:10 -0700

    Time: 67 ms (81.34%), Space: 43.3 MB (50.50%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.js
+++ 0042-trapping-rain-water/0042-trapping-rain-water.js
@@ -3,21 +3,32 @@
  * @return {number}
  */
 var trap = function(height) {
-    let L = 0; 
+    // 2 pointers 
+    // keep track of max height from the left and right 
+    // keep track of total trapped water 
+    // we move the pointers based on which pointer is greater 
+    // as the pointer moves, we 
+        // update max if the height is greater 
+        // if height is less, we take the difference and add to total 
+    let L = 0;
     let R = height.length - 1;
-    let lMax = height[L];
-    let rMax = height[R];
-    let water = 0;
+    let leftMax = height[L];
+    let rightMax = height[R];
+    let total = 0;
     while (L < R) {
-        if (height[L] > height[R]) {
-            water += rMax - height[R];
+        if (leftMax > rightMax) {
             R -= 1;
-            rMax = Math.max(rMax, height[R]);
+            if (height[R] < rightMax) {
+                total += (rightMax - height[R]);
+            }
+            rightMax = Math.max(rightMax, height[R]);
         } else {
-            water += lMax - height[L];
             L += 1;
-            lMax = Math.max(lMax, height[L]);
+            if (height[L] < leftMax) {
+                total += (leftMax - height[L]);
+            }
+            leftMax = Math.max(leftMax, height[L]);
         }
     }
-    return water;
+    return total;
 };
