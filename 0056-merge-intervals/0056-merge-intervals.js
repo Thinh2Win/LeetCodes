0ae6afa21685e4e59bac5f7f403983e2b6ed133c
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-03-14 20:33:31 -0700

    Time: 107 ms (54.32%), Space: 49.6 MB (31.14%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.js
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -3,13 +3,25 @@
  * @return {number[][]}
  */
 var merge = function(intervals) {
-    intervals.sort((a, b) => a[0] - b[0]);
+    // sort intervals [s1, e1], [s2, e2]
+    // overlap if e1 >= s2 
+    // when we merge we take min of s1 and s2
+    // and the max of e1 and e2 => [min, max]
+    intervals.sort((a, b) => {
+        if (a[0] === b[0]) {
+            return a[1] - b[1];
+        } else {
+            return a[0] - b[0];
+        }
+    });
     let answer = [intervals[0]];
     for (let i = 1; i < intervals.length; i++) {
-        if (intervals[i][0] <= answer.at(-1)[1]) {
-            answer[answer.length - 1] = [Math.min(intervals[i][0], answer.at(-1)[0]), Math.max(intervals[i][1], answer.at(-1)[1])];
+        let prev = answer.at(-1);
+        let curr = intervals[i];
+        if (prev[1] >= curr[0]) {
+            answer[answer.length - 1] = [Math.min(prev[0], curr[0]), Math.max(prev[1], curr[1])]
         } else {
-            answer.push(intervals[i]);
+            answer.push(curr);
         }
     }
     return answer;
