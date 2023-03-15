ea4ce58c2ba9a93a8d2a01bfe68700afd757c562
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-03-15 16:57:39 -0700

    Time: 71 ms (57.87%), Space: 44.6 MB (45.67%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.js
+++ 0101-symmetric-tree/0101-symmetric-tree.js
@@ -11,28 +11,19 @@
  * @return {boolean}
  */
 var isSymmetric = function(root) {
-    let q = [root];
-    while (q.length) {
-        let level = [];
-        for (let i = q.length; i > 0; i--) {
-            let node = q.shift();
-            if (node.left) {
-                level.push(node.left.val);
-                q.push(node.left);
-            } else {
-                level.push(null);
-            }
-            if (node.right) {
-                level.push(node.right.val);
-                q.push(node.right);
-            } else {
-                level.push(null);
-            }
+    if (!root) return true;
+    const isSame = (leftNode, rightNode) => {
+        if (!leftNode && !rightNode) {
+            return true;
+        } 
+        if (leftNode && !rightNode || !leftNode && rightNode) {
+            return false;
         }
-        let mp = level.length / 2;
-        let left = level.slice(0, mp);
-        let right = level.slice(mp);
-        if (JSON.stringify(left) !== JSON.stringify(right.reverse())) return false;
+        if (leftNode && rightNode) {
+            if (leftNode.val !== rightNode.val) return false;
+            return (isSame(leftNode.left, rightNode.right) && isSame(leftNode.right, rightNode.left))
+        }
+        return true;
     }
-    return true;
+    return isSame(root.left, root.right);
 };
