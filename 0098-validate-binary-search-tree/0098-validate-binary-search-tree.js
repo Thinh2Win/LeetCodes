bd59c529195c8e1ff18e4e810ca7bf320c8fadfd
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-03-15 16:22:19 -0700

    Time: 64 ms (91.46%), Space: 46.8 MB (24.36%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
@@ -11,16 +11,16 @@
  * @return {boolean}
  */
 var isValidBST = function(root) {
-    let isValid = true;
+    if (!root) return false;
+    let valid = true;
     const DFS = (node, leftMin, rightMax) => {
-        if (!isValid) return;
-        if (node.val >= leftMin || node.val <= rightMax) {
-            isValid = false;
+        if (node.val >= leftMin || node.val <= rightMax ) {
+            valid = false;
             return;
         }
         node.left && DFS(node.left, Math.min(leftMin, node.val), rightMax);
         node.right && DFS(node.right, leftMin, Math.max(rightMax, node.val));
-    }
+    };
     DFS(root, Infinity, -Infinity);
-    return isValid;
+    return valid;
 };
