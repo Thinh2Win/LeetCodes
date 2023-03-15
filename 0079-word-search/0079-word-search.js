0e4c08950b1a44f0afad636c204d37cb19d92062
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-03-15 12:01:21 -0700

    Time: 437 ms (96.21%), Space: 49 MB (13.40%) - LeetHub

--- 0079-word-search/0079-word-search.js
+++ 0079-word-search/0079-word-search.js
@@ -3,42 +3,41 @@
  * @param {string} word
  * @return {boolean}
  */
-var exist = function(board, word) {
-    let hasWord = false;
-        
-         const DFS = (idx, position) => {
-            let [row, col] = position;
-             if (hasWord) return true; 
-            if (idx === word.length - 1) {
-                hasWord = true;
-                return;
-            }
-            board[row][col] = 1;
-            //check up
-            if (row - 1 >= 0 && board[row - 1][col] === word[idx + 1]) {
-                DFS(idx + 1, [row - 1, col]);
-            }
-            // check left
-            if (col - 1 >= 0 && board[row][col - 1] === word[idx + 1]) {
-                DFS(idx + 1, [row, col - 1]);
-            }
-            // check down 
-            if (row + 1 < board.length && board[row + 1][col] === word[idx + 1]) {
-                DFS(idx + 1, [row + 1, col]);
-            }
-            // check right 
-            if (col + 1 < board[row].length && board[row][col + 1] === word[idx + 1]) {
-                DFS(idx + 1, [row, col + 1]);
-            }
-             board[row][col] = word[idx];
-        }
-     for (let row = 0; row < board.length; row++) {
+var exist = function(board, word) { 
+    // find all locations of first letter of word
+    // then do bfs from locations to check if the next following letters exist 
+    if (word.length > (board.length * board[0].length)) return false;
+    let locations = [];
+    for (let row = 0; row < board.length; row++) {
         for (let col = 0; col < board[row].length; col++) {
             if (board[row][col] === word[0]) {
-                DFS(0, [row, col]); 
-                if (hasWord) return true;
+                locations.push([row, col]);
             }
         }
     }
-    return hasWord; 
+    const BFS = (idx, [row, col]) => {
+        if (idx === word.length) {
+            return true;
+        }
+        board[row][col] = '*'
+        let letter = word[idx];
+        if (row - 1 >= 0 && board[row - 1][col] === letter) {
+            var up = BFS(idx + 1, [row - 1, col]) || false;
+        }
+         if (col + 1 < board[row].length && board[row][col + 1] === letter) {
+            var right = BFS(idx + 1, [row, col + 1]) || false;
+        }
+         if (row + 1 < board.length && board[row + 1][col] === letter) {
+             var down = BFS(idx + 1, [row + 1, col]) || false;
+        }
+         if (col - 1 >= 0 && board[row][col - 1] === letter) {
+            var left = BFS(idx + 1, [row, col - 1]) || false;
+        }
+        board[row][col] = word[idx - 1];
+        return (up || left || down || right);
+    };
+    for (let i = 0; i < locations.length; i++) {
+        if (BFS(1, locations[i])) return true;    
+    };
+    return false;
 };
