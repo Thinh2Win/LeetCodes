f46d7999963d43a7988e5e1799ab3954be684b2a
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-03-14 20:42:01 -0700

    Time: 54 ms (93.84%), Space: 42.3 MB (42.20%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.js
+++ 0062-unique-paths/0062-unique-paths.js
@@ -4,17 +4,17 @@
  * @return {number}
  */
 var uniquePaths = function(m, n) {
-    if (m === 1 && n === 1) return 1;
     let matrix = new Array(m).fill(0).map((zero, idx) => {
         if (idx === 0) {
             return new Array(n).fill(1);
+        } else {
+            let row = new Array(n).fill(0);
+            row[0] = 1;
+            return row;
         }
-        let row = new Array(n).fill(0);
-        row[0] = 1;
-        return row;
     });
-    for (let row = 1; row < matrix.length; row++) {
-        for (let col = 1; col < matrix[row].length; col++) {
+    for (let row = 1; row < m; row++) {
+        for (let col = 1; col < n; col++) {
             matrix[row][col] = matrix[row - 1][col] + matrix[row][col - 1];
         }
     }
