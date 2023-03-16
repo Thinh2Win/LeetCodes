a6c60ad75bafa3071c3ef513fdeaa8efa4b0119d
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-03-16 14:31:22 -0700

    Time: 103 ms (45.94%), Space: 82.2 MB (42.34%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.js
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.js
@@ -12,11 +12,11 @@
  * @return {TreeNode}
  */
 var buildTree = function(inorder, postorder) {
-        if (!inorder.length) return null;
-        let val = postorder.pop();
-        let idx = inorder.indexOf(val);
-        let node = new TreeNode(val);
-        node.right = buildTree(inorder.slice(idx + 1), postorder);
-        node.left = buildTree(inorder.slice(0, idx), postorder);
-        return node;
+    if (!inorder.length) return null;
+    let val = postorder.pop();
+    let node = new TreeNode(val);
+    let idx = inorder.indexOf(val);
+    node.right = buildTree(inorder.slice(idx + 1), postorder);
+    node.left = buildTree(inorder.slice(0, idx), postorder);
+    return node;
 };
