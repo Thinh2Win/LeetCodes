f28447c7a6a303749eca38b02b2f4a2888e19cce
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-03-16 16:15:59 -0700

    Time: 58 ms (88.70%), Space: 42.5 MB (27.53%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
@@ -4,9 +4,9 @@
  */
 var maxProfit = function(prices) {
     let profit = 0;
-    for (let i = 0; i < prices.length; i++) {
+    for (let i = 0; i < prices.length - 1; i++) {
         if (prices[i] < prices[i + 1]) {
-            profit += prices[i + 1] - prices[i];
+            profit += (prices[i + 1] - prices[i]);
         }
     }
     return profit;
