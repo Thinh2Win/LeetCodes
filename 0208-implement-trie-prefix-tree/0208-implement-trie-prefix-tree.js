96ffcb8516b907797b34ac9a43fab38b468fb709
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-03-17 08:42:21 -0700

    Time: 190 ms (83.15%), Space: 56.2 MB (92.79%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.js
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.js
@@ -1,36 +1,36 @@
 
 class Trie {
-    constructor () {
+    constructor() {
         this.root = {};
     }
-    
+
     insert(word) {
         let node = this.root;
         for (let c of word) {
-          if (!node[c]) node[c] = {};
-          node = node[c];
+            if (!node[c]) node[c] = {};
+            node = node[c];
         }
         node.isWord = true;
-    };
+    }
 
     traverse(word) {
         let node = this.root;
         for (let c of word) {
             node = node[c];
-            if (!node) return null; 
+            if (!node) return null;
         }
         return node;
     }
 
     search(word) {
-        const node = this.traverse(word);
+        let node = this.traverse(word);
         return node !== null && node.isWord === true;
-    };
+    }
 
     startsWith(prefix) {
         return this.traverse(prefix) !== null;
-    };
-};
+    }
+}
 
 
 
