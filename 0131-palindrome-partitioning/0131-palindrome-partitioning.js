d6263b954bb47d02d3dcc543a55827e89f1bb9d5
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-03-17 09:13:28 -0700

    Time: 308 ms (50.66%), Space: 68.4 MB (50.14%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.js
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.js
@@ -2,32 +2,35 @@
  * @param {string} s
  * @return {string[][]}
  */
-const isPalindrome = (string) => {
-    if (string.length === 1) return true;
-    let L = 0;
-    let R = string.length - 1;
-    while (L < R) {
-        if (string[L] !== string[R]) return false;
-        L += 1; 
-        R -= 1;
-    }
-    return true;
-}
 var partition = function(s) {
-    let answer = [];
+    let answer = new Set();
     const DFS = (idx, string, container) => {
         if (idx === s.length) {
-            if (container.join('').length === s.length) {
-                answer.push(container);
+            if (string.length === 0) {
+                answer.add(container);    
             }
             return;
         }
         string += s[idx];
         if (isPalindrome(string)) {
             DFS(idx + 1, '', [...container, string]);
-        } 
+        }
         DFS(idx + 1, string, container);
     };
     DFS(0, '', []);
-    return answer;
+    return [...answer];
 };
+
+function isPalindrome(word) {
+    if (word.length === 1) return true;
+    let L = 0;
+    let R = word.length - 1;
+    while (L < R) {
+        if (word[L] !== word[R]) {
+            return false;
+        };
+        L += 1;
+        R -= 1;
+    }
+    return true;
+}
