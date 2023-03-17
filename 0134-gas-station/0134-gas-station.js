9575b05a60ea1fe5446b9e87061836d0224b5cad
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-03-17 09:43:08 -0700

    Time: 85 ms (65.71%), Space: 51.7 MB (89.24%) - LeetHub

--- 0134-gas-station/0134-gas-station.js
+++ 0134-gas-station/0134-gas-station.js
@@ -4,23 +4,18 @@
  * @return {number}
  */
 var canCompleteCircuit = function(gas, cost) {
-    let gTot = 0;
-    let cTot = 0;
-    cost.forEach((x, idx) => {
-       gTot += gas[idx];
-        cTot += x;
-    });
-    if (gTot < cTot) return -1;
-    let total = 0;
-    let least = 0;
-    let start = 0; 
-    gas.forEach((station, idx) => {
-        total += station;
-        total -= cost[idx];
-        if (total < least) {
-            least = total;
-            start = idx + 1;
+    let totalGas = gas.reduce((a, b) => a + b, 0);
+    let totalDistance = cost.reduce((a, b) => a + b, 0);
+    if (totalGas < totalDistance) return -1;
+    let min = [Infinity, null];
+    let curr = 0;
+    for (let i = 0; i < gas.length; i++) {
+        curr += gas[i];
+        curr -= cost[i];
+        if (curr < min[0]) {
+            min[0] = curr;
+            min[1] = i + 1 === gas.length ? 0 : i + 1;
         }
-    });
-    return start;
+    }
+    return min[1];
 };
