b271651355e9d1ca96af3b28c30dbdf65fd10137
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-03-16 18:50:54 -0700

    Time: 93 ms (96.64%), Space: 57.1 MB (73.33%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -4,29 +4,17 @@
  */
 var longestConsecutive = function(nums) {
     if (!nums.length) return 0;
-    let map = {};
-    nums.forEach(num => {
-        map[num - 1] ? map[num - 1].push(num) : map[num - 1] = [num];
-        map[num + 1] ? map[num + 1].push(num) : map[num + 1] = [num];
-    });
-    let seen = {};
-    let max = 1;
-    let depth = 1;
-    const DFS = (num) => {
-        if (seen[num]) return;
-        seen[num] = true;
-        max = Math.max(max, depth);
-        let connections = map[num] || [];
-        for (let j = 0; j < connections.length; j++) {
-            if (seen[connections[j]]) continue;
-            depth += 1
-            DFS(connections[j]);
+    let set = new Set(nums);
+    let max = 0;
+    for (let num of set) {
+        if (set.has(num - 1)) continue;
+        let currMax = 1;
+        let curr = num;
+        while (set.has(curr + 1)) {
+            currMax += 1;
+            curr += 1;
         }
+        max = Math.max(max, currMax);
     }
-    for (let i = 0; i < nums.length; i++) {
-        if (seen[nums[i]]) continue;
-        depth = 1;
-        DFS(nums[i]);
-    };
     return max;
 };
