43df61017894d72dd734789140ad5ca48f4115ad
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-04-06 13:39:23 -0700

    Time: 140 ms (15.59%), Space: 58.4 MB (5.31%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -2,43 +2,34 @@
  * @param {character[][]} grid
  * @return {number}
  */
-var numIslands = function(grid) {
-      // iterate through grid 
-    // if we find a 1 we do BFS and change all attatched 1's to 0's 
-    // increment counter 
-    // return counter 
-    let counter = 0;
-    const BFS = ([row, col]) => {
-    let q = [[row, col]];
-    grid[row][col] = 0;
-    while (q.length) {
-        let [row, col] = q.shift();
-        if (row - 1 >= 0 && grid[row - 1][col] === '1') {
-            grid[row - 1][col] = 0;
-            q.push([row - 1, col]);
-        }
-        if (row + 1 < grid.length && grid[row + 1][col] === '1') {
-            grid[row + 1][col] = 0;
-            q.push([row + 1, col]);
-        }
-        if (col - 1 >= 0 && grid[row][col - 1] === '1') {
-            grid[row][col - 1] = 0;
-            q.push([row, col - 1]);
-        }
-        if (col + 1 >= 0 && grid[row][col + 1] === '1') {
-            grid[row][col + 1] = 0;
-            q.push([row, col + 1]);
-        }
-    }
-}
-
+var numIslands = function(grid) { 
+    let coordinates = [];
     for (let row = 0; row < grid.length; row++) {
         for (let col = 0; col < grid[row].length; col++) {
             if (grid[row][col] === '1') {
-                counter += 1;
-                BFS([row, col]);
+                coordinates.push([row, col]);
             }
         }
     }
-    return counter;
+    
+    const BFS = (coordinate) => {
+        let [row, col] = coordinate;
+        grid[row][col] = '0';
+        let directions = [[1, 0], [-1, 0], [0, -1], [0, 1]];
+        directions.forEach(direction => {
+            let [x, y] = direction;
+            if (grid[row + x]?.[col + y] === '1') {
+                BFS([row + x, col + y]);
+            }
+        });
+        return 1;
+    }
+    
+    let count = 0;
+    for (let i = 0; i < coordinates.length; i++) {
+        let [row, col] = coordinates[i];
+        if (grid[row][col] === '0') continue;
+        count += BFS(coordinates[i]);
+    }
+    return count;
 };
