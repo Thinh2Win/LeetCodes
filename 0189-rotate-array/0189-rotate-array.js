55664e2297ce4df625552a9eaf6ad6e991b3a22e
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-04-06 12:16:59 -0700

    Time: 75 ms (99.29%), Space: 51.5 MB (71.99%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.js
+++ 0189-rotate-array/0189-rotate-array.js
@@ -7,9 +7,6 @@ var rotate = function(nums, k) {
     let rotations = k % nums.length;
     if (rotations === 0) return nums;
     let end = nums.slice(nums.length - rotations);
-    while (rotations > 0) {
-        nums.pop();
-        rotations -= 1;
-    }
+    nums.splice(nums.length - rotations, rotations)
     nums.unshift(...end);
 };
