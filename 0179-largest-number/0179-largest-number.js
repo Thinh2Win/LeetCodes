6e4d7597f21a96fe406fbb6bceaef5b3bf24ae26
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-04-06 12:03:05 -0700

    Time: 72 ms (35.19%), Space: 43.9 MB (73.46%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {string}
+ */
+var largestNumber = function(nums) {
+    if (!nums.length) return '0';
+    nums.sort((a, b) => `${b}${a}` - `${a}${b}`);
+    if (nums[0] === 0) return '0';
+    return nums.join('');
+};
