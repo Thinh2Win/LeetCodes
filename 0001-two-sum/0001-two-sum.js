9388288728e3a5fe1f49865daf7d303a8446d68a
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-04-07 14:26:14 -0700

    Time: 72 ms (66.72%), Space: 42.8 MB (42.66%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -6,8 +6,10 @@
 var twoSum = function(nums, target) {
     let map = {};
     for (let i = 0; i < nums.length; i++) {
+        if (map[nums[i]] !== undefined) {
+            return [map[nums[i]], i];
+        }
         let diff = target - nums[i];
-        if (map[nums[i]] !== undefined) return [map[nums[i]], i];
         map[diff] = i;
     }
 };
