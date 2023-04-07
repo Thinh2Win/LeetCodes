fca8775a6d8b5925db67ad8ceec95b39afad5c58
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-04-07 16:07:18 -0700

    Time: 214 ms (21.94%), Space: 49.1 MB (10.49%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -4,18 +4,30 @@
  * @return {number}
  */
 var characterReplacement = function(s, k) {
-    let freqChar = 0;
+    // sliding window 
+    // create L and R pointer 
+    // create a map to track letters inside window
+    // track most frequent letter 
+    // if the size of the window - most frequent letter is > k then we know that there are more unique letters inside our window than allowed 
+        // in this case we shrink our window until window - letter is < or equal to k 
     let L = 0;
     let map = {};
-    let max = -Infinity;
-    for (var R = 0; R < s.length; R++) {
-        map[s[R]] ? map[s[R]] += 1 : map[s[R]] = 1;
-        freqChar = Math.max(freqChar, map[s[R]]);
-        while (R - L - freqChar + 1 > k) {
+    let freq = 0;
+    let max = 0;
+    for (let R = 0; R < s.length; R++) {
+        if (map[s[R]] !== undefined) {
+            map[s[R]] += 1;
+            freq = Math.max(freq, map[s[R]]);
+        } else {
+            map[s[R]] = 1;
+            freq = Math.max(freq, map[s[R]]);
+        }
+        while ((R - L + 1) - freq > k) {
             map[s[L]] -= 1;
             L += 1;
+            freq = Math.max(...Object.values(map));
         }
         max = Math.max(max, R - L + 1);
     }
-    return max === -Infinity ? s.length : max;
+    return max;
 };
