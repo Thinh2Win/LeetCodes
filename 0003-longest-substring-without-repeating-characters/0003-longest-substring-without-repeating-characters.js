73757f0d38f80e8ae16e1c612d6fae8d19d0f80e
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-04-07 15:52:20 -0700

    Time: 99 ms (47.43%), Space: 49.1 MB (21.96%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,12 +3,19 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    let map = {};
+    // sliding window 
+    // create L and R pointer 
+    // create a map to keep track of letters in our window 
+    // if a letter is ever greater than or equal to 2 
+    // we need to shrink our window until that letter is less than 2
+    // keep track of longest window length 
     let max = 0;
     let L = 0;
+    let map = {};
     for (let R = 0; R < s.length; R++) {
-        map[s[R]] !== undefined ? map[s[R]] += 1 : map[s[R]] = 1;
-        while (map[s[R]] > 1) {
+        let char = s[R];
+        map[char] !== undefined ? map[char] += 1 : map[char] = 1;
+        while (map[char] >= 2) {
             map[s[L]] -= 1;
             L += 1;
         }
