b03185c28add25a6ffa7e6ca51533a3894426445
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-04-08 11:43:00 -0700

    Time: 89 ms (39.61%), Space: 45.1 MB (76.52%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.js
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.js
@@ -4,21 +4,8 @@
  * @return {number}
  */
 var minEatingSpeed = function(piles, h) {
-    // binary search 
-    // need to find our range 
-    // need to figure out the conditions to determine which half of our range we can remove 
-    // range related to bph (bannas per hour)
-    // min = 1 max range = total bannas in the pile 
-    // piles = [3,6,7,11] min = 1 max = 27
-    // mp = 13 bph 
-    // so in order to determine which side of the mp to remove 
-    // we need to know if koko eats 13 bph, will that be enough to finish the pile within h hours 
-    // since koko can eat each pile in 1 hour, she only used 4 out of 8 hours 
-    // since we know she can finish the pile, we can remove the right side of the mp 
-    // if she was unable to finish the pile we remove the left 
-    // once our binary search finishes, we're left with the minimum bph koko can eat to satisfy the conditions
     let min = 1;
-    let max = piles.reduce((a, b) => a + b, 0);
+    let max = Math.max(...piles);
     while (min < max) {
         let bph = Math.floor((max - min) / 2) + min;
         if (isPossible(bph, piles, h)) {
@@ -32,8 +19,10 @@ var minEatingSpeed = function(piles, h) {
 
 function isPossible(bph, piles, h) {
     let hoursLeft = h;
-    piles.forEach(pile => {
-       hoursLeft -= Math.ceil(pile / bph); 
-    });
-    return hoursLeft >= 0;
+    for (let i = 0; i < piles.length; i++) {
+        let pile = piles[i];
+        hoursLeft -= Math.ceil(pile / bph);  
+        if (hoursLeft < 0) return false;
+    }
+    return true;
 }
