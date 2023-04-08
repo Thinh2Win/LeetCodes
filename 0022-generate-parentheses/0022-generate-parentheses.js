03d83080c93d51fedbd2d2519f625b1d9f5a4356
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-04-08 09:27:11 -0700

    Time: 53 ms (93.35%), Space: 42.3 MB (85.33%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.js
+++ 0022-generate-parentheses/0022-generate-parentheses.js
@@ -3,18 +3,26 @@
  * @return {string[]}
  */
 var generateParenthesis = function(n) {
+    // if n = 3
+    // we have 3 opening and 3 closing parenthesis 
+    // decision tree 
+    // we can either start with an opening or closing bracket 
+    // but we can only use a closing bracket as long as the number of closing brackets is higher than opening brackets 
+    // so we use recursion with a decision tree to decide which path to take 
+    // opening vs. closing bracket used 
+    // break case is when the number of closing and opening brackets is zero
+    // if we reach break case we push our result into an answer array 
+    // so recursive function will keep track of 
+        // opening and closing parenthesis 
+        // and current string formed 
     let answer = [];
     const recurse = (open, closing, string) => {
         if (open === 0 && closing === 0) {
             answer.push(string);
             return;
         }
-        if (open !== 0) {
-            recurse(open - 1, closing, string + '(');
-        }
-        if (closing !== 0 && closing > open) {
-            recurse(open, closing - 1, string + ')');
-        }
+        if (open > 0) recurse(open - 1, closing, string + '(');
+        if (closing > open) recurse(open, closing - 1, string + ')');
     };
     recurse(n, n, '');
     return answer;
