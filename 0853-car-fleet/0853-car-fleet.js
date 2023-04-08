d7212f1578f7c9c58109c6d36af4759ad4f1d1cc
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-04-08 11:00:54 -0700

    Time: 317 ms (47.22%), Space: 70.6 MB (54.37%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.js
+++ 0853-car-fleet/0853-car-fleet.js
@@ -5,15 +5,7 @@
  * @return {number}
  */
 var carFleet = function(target, position, speed) {
-    // [10,8,0,5,3]
-    // [0, 3, 5, 8, 10]
-    // to get rotations = (target - position) / speed
-    // probably have to map the positions to their speeds 
-    // [12, 3, 7, 1, 1] rotations needed to reach target
-    // starting from right to left 
-    // we know 2 cars become a fleet when 
-        // number of rotations of the car to its left is less than or equal to its own rotations b/c 
-        // this tells us that the car behind will always catch up and join the fleet if the number of its rotations is less than or equal to the number of rotations of the car in front 
+    
     let map = {};
     position.forEach((car, idx) => {
        map[car] = speed[idx]; 
@@ -23,8 +15,7 @@ var carFleet = function(target, position, speed) {
         return (target - car) / map[car];
     });
     let fleets = 0;
-    rotations.unshift(Infinity);
-    while (rotations.at(-1) !== Infinity) {
+    while (rotations.length) {
         let start = rotations.pop();
         while (start >= rotations.at(-1)) {
             rotations.pop();
