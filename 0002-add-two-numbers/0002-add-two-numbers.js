b1f6b3ec36b3ac08dc569eea8edc44b758d0268d
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-04-08 15:31:30 -0700

    Time: 110 ms (40.69%), Space: 47.9 MB (24.53%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.js
+++ 0002-add-two-numbers/0002-add-two-numbers.js
@@ -11,32 +11,28 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-    let head;
+    let node1 = l1;
+    let node2 = l2;
+    let head = new ListNode(0);
+    let link = head;
     let carry = 0;
-    if (l1.val + l2.val > 9) {
-        carry = 1;
-        head = new ListNode(+(`${l1.val + l2.val}`[1]));
-    } else {
-        head = new ListNode(l1.val + l2.val);
-    }
-    let curr = head;
-    let n1 = l1.next;
-    let n2 = l2.next; 
-    while (n1 || n2) {
-        let sum = (n1?.val || 0) + (n2?.val || 0) + carry;
+    while (node1 || node2) {
+        let num1 = node1?.val ?? 0;
+        let num2 = node2?.val ?? 0;
+        let sum = num1 + num2 + carry;
         if (sum > 9) {
-            curr.next = new ListNode(+(`${sum}`[1]));
             carry = 1;
+            link.next = new ListNode(+(`${sum}`[1]));
         } else {
-            curr.next = new ListNode(sum);
+            link.next = new ListNode(sum);
             carry = 0;
         }
-        curr = curr.next;
-        if (n1) n1 = n1.next;
-        if (n2) n2 = n2.next;
+        link = link.next;
+        node1 && (node1 = node1.next);
+        node2 && (node2 = node2.next);
     }
     if (carry === 1) {
-        curr.next = new ListNode(1);
+        link.next = new ListNode(1);
     }
-    return head;
+    return head.next;
 };
