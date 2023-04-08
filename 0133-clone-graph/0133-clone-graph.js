37b09154f7e0e01900286bd04f880a6b1654539e
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-04-08 08:47:59 -0700

    Time: 56 ms (91.60%), Space: 44.2 MB (15.70%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.js
@@ -0,0 +1,32 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, neighbors) {
+ *    this.val = val === undefined ? 0 : val;
+ *    this.neighbors = neighbors === undefined ? [] : neighbors;
+ * };
+ */
+
+/**
+ * @param {Node} node
+ * @return {Node}
+ */
+var cloneGraph = function(node) {
+  let map = new Map();
+    let start = node;
+    if (start === null) {
+        return null;
+    }
+    let q = [start];
+    map.set(start, new Node(start.val));
+    while ( q.length > 0) {
+        let currentNode = q.shift();
+        currentNode.neighbors.forEach(neighbor => {
+            if (!map.has(neighbor)) {
+                map.set(neighbor, new Node(neighbor.val));
+                q.push(neighbor); 
+            }
+            map.get(currentNode).neighbors.push(map.get(neighbor)); 
+        });
+    }
+    return map.get(start);
+};
