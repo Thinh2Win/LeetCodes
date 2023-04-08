d1a9f8ff362a11f69d237d4833ac172c0d4da3f5
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-04-08 15:59:38 -0700

    Time: 78 ms (90.16%), Space: 49.7 MB (82.35%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
@@ -4,17 +4,17 @@
  */
 var findDuplicate = function(nums) {
     let slow = nums[0];
-    let fast = nums[nums[0]];
-    while (slow !== fast) {
+    let fast = nums[0];
+    
+    while (true) {
         slow = nums[slow];
         fast = nums[nums[fast]];
+        if (slow === fast) break;
     }
-    slow = 0;
-    
+    fast = nums[0];
     while (slow !== fast) {
         slow = nums[slow];
         fast = nums[fast];
-    }
-    
+    };
     return slow;
 };
