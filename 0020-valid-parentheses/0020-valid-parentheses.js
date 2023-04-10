5ad973d2f5424e362364378156b474552fa4f050
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-04-10 07:51:40 -0700

    Time: 59 ms (83.45%), Space: 41.5 MB (98.70%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -2,25 +2,42 @@
  * @param {string} s
  * @return {boolean}
  */
-const map = {
-    '[': true,
-    '(': true,
-    '{': true,
-    ']': '[',
-    ')': '(',
-    '}': '{',
-}
-
 var isValid = function(s) {
+    // input parenthesis () [] {} 
+    // output: boolean on whether given string is a valid parenthesis 
+    // constraints: time? space? 
+    // edge: 
+        // given anything besides parenthesis? numbers or letters? 
+        // will the given string ever be empty? 
+    
+    //strategy 
+    // everytime we see an opening parenthesis ( [ {, the next clossing parenthesis has to be a matching closing parenthesis (stack)
+    // ( -> )  "()[]" "([{}])"
+    // keep track of most recent opening parenthesis (top of stack)
+    // once we reach a clossing parenthesis, check if they're a match 
+    // map out closing parenthesis matching opening bracket 
+    // if not we return false 
+    // if they are we continue 
+    // once we comfirm that all parenthesis have a matching partner 
+        // (checking if stack is empty)
+    // we can return true 
+    
     let stack = [];
+    let map = {
+        '(': true,
+        '[': true,
+        '{': true,
+        '}': '{',
+        ']': '[',
+        ')': '(',
+    };
     for (let i = 0; i < s.length; i++) {
         if (map[s[i]] === true) {
             stack.push(s[i]);
         } else {
-            let check = stack.pop();
-            if (map[s[i]] !== check) return false;
+            let openingBracket = stack.pop();
+            if (map[s[i]] !== openingBracket) return false;
         }
     }
-    if (stack.length) return false;
-    return true;
+    return stack.length ? false : true;
 };
