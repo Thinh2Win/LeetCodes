07ba6a9e49d7675be4f07388dab831ba889ab200
Thinh Ngo Phan <ngophanthinh@gmail.com>
2025-03-03 15:16:47 -0800

    Time: 0 ms (100%), Space: 57.5 MB (15.9%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.js
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.js
@@ -10,18 +10,34 @@
  * @return {ListNode}
  */
 var oddEvenList = function(head) {
-    if (!head) return null;
-    let oddHead = head;
-    let evenHead = head.next;
-    let odd = oddHead;
-    let even = evenHead;
-    while (even) {
-        odd.next = odd.next?.next;   
-        if (odd.next) odd = odd.next;
-        
-        if (even.next) even.next = even.next?.next;
-        even = even.next;
-    };
-    odd.next = evenHead;
-    return oddHead;
+    if (!head) return head;
+    // create a pointer for odd links and a pointer for even links 
+    // odd pointer will be head 
+    let odd = head;
+    let evenHead = new ListNode(0);
+    let even = evenHead; 
+    let prev = head;
+    let currNode = head.next;
+    let isOdd = false;
+    // keep track of even or odd link while traversing linked list 
+    while (currNode) {
+        if (isOdd) {
+            odd.next = currNode;
+            odd = odd.next;
+           
+        } else { // its even
+            even.next = currNode;
+            even = even.next;
+        }
+        prev.next = null;
+        prev = currNode;
+        currNode = currNode.next
+        isOdd = !isOdd;
+    }
+    // as we traverse we attach even links to even pointer and odd links to odd pointer
+    // connect odd list and even list after traversal 
+    odd.next = evenHead.next;
+    // return head 
+    return head;
+
 };
