916065c6d73bd7e8bb4d7e645875ae60a49bce1d
Thinh Ngo Phan <ngophanthinh@gmail.com>
2025-03-03 12:19:55 -0800

    Time: 37 ms (56.16%), Space: 68.5 MB (20.07%) - LeetHub

---
+++ 0969-number-of-recent-calls/0969-number-of-recent-calls.js
@@ -0,0 +1,23 @@
+
+var RecentCounter = function() {
+    this.queue = [];
+};
+
+/** 
+ * @param {number} t
+ * @return {number}
+ */
+RecentCounter.prototype.ping = function(t) {
+    this.queue.unshift(t);
+    let end = t - 3000;
+    while (this.queue.at(-1) < end) {
+        this.queue.pop();
+    } 
+    return this.queue.length;
+};
+
+/** 
+ * Your RecentCounter object will be instantiated and called as such:
+ * var obj = new RecentCounter()
+ * var param_1 = obj.ping(t)
+ */
