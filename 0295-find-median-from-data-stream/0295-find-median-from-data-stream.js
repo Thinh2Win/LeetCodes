6bd857d280d5e3ba1c0db156a501fe423b919a2a
Thinh Ngo Phan <ngophanthinh@gmail.com>
2025-03-07 11:40:32 -0800

    Time: 129 ms (78.6%), Space: 89.6 MB (12.05%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.js
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.js
@@ -1,8 +1,55 @@
 
+/* 
+    by using a min and max heap we can effectively track the mp of the nums array 
+    ex. say we have [1, 2, 3, 4, 5, 6]
+    [1, 2, 3] [4, 5, 6] -> the mp is the max of the max heap (left )and the min of the min heap (right)
+    [1, 2] [3, 4, 5] -> with odd length, mp depends on which heap is greater, 
+    if min heap greater length, then mp is min else max heap is greater length so mp is max 
+
+    so essentially, we don't have to worry about sorting the array as numbers stream in
+    we just need to keep track of the max from max heap and min from min heap 
+    but to do this we need to split the numbers evenly between the two as they stream in 
+    if nums came in as [2, 6, 3, 4, 1, 5]
+
+    then step by step we'd have 
+    1) insert 2
+        maxHeap: [2]
+        minHeap: []  
+    2) insert 6
+        maxHeap: [6, 2]
+        minHeap: []
+
+        since length of max is 2 more than min we transfer top of max to min 
+        maxHeap: [2]
+        minHeap: [6]
+
+    3) insert 3
+    maxHeap: [3, 2]
+    minHeap: [6] 
+
+    4) insert 4
+    maxHeap: [4, 3, 2]
+    minHeap: [6]  
+    rearrange
+    maxHeap: [3, 2]
+    minHeap: [4, 6]  
+
+    5) insert 1
+    maxHeap: [3, 2, 1]
+    minHeap: [4, 6]  
+
+    6) insert 5
+    maxHeap: [5, 3, 2, 1]
+    minHeap: [4, 6]  
+    rearrange
+    maxHeap: [3, 2, 1]
+    minHeap: [4, 6, 5]  
+    
+    so now we have our max pointing to 3 and min pointing to 4 which are our 2 mps 
+*/
 var MedianFinder = function() {
-    this.arr = [],
-    this.min = 0,
-    this.max = -Infinity
+    this.maxQ = new MaxPriorityQueue();
+    this.minQ = new MinPriorityQueue();
 };
 
 /** 
@@ -10,46 +57,35 @@ var MedianFinder = function() {
  * @return {void}
  */
 MedianFinder.prototype.addNum = function(num) {
-    let mp = Math.floor(this.arr.length / 2);
-    if (this.arr.length < 3) {
-        this.arr.push(num);
-        this.arr.sort((a, b) => a - b);
-        this.max = this.arr.length - 1;
-    } else if (num >= this.arr[this.max]) {
-        this.arr.push(num);
-        this.max += 1;
-    } else if (num <= this.arr[this.min]) {
-        this.arr.unshift(num);
-        this.max += 1;
-    } else if (num > this.arr[this.min] && num <= this.arr[mp]) {
-       for (let i = this.min; i < mp; i++) {
-           if (this.arr[i + 1] >= num) {
-               this.arr.splice(i + 1, 0, num);
-               break;
-           }
-       }
-        this.max += 1;
-    } else if (num < this.arr[this.max] && num >= this.arr[mp]) {
-        for (let j = this.max; j > mp; j--) {
-            if (this.arr[j - 1] <= num) {
-                this.arr.splice(j, 0, num);
-                break;
-            }
-        }
-        this.max += 1;
+    this.maxQ.enqueue(num);
+
+    if (this.maxQ.front() > this.minQ.front()) {
+        this.minQ.enqueue(this.maxQ.dequeue())
     }
-    return this.arr;
+
+    if (this.maxQ.size() - this.minQ.size() >= 2) {
+        // rearrange
+        this.minQ.enqueue(this.maxQ.dequeue());
+    } else if (this.minQ.size() - this.maxQ.size() >= 2) {
+        this.maxQ.enqueue(this.minQ.dequeue());
+    }
 };
 
 /**
  * @return {number}
  */
 MedianFinder.prototype.findMedian = function() {
-    let mp = Math.floor(this.arr.length / 2);
-    if (this.arr.length % 2 === 0) {
-        return (this.arr[mp] + this.arr[mp - 1]) / 2
+    let minLength = this.minQ.size();
+    let maxLength = this.maxQ.size();
+    if (maxLength > minLength) {
+        return this.maxQ.front();
+    } else if (minLength > maxLength) {
+        return this.minQ.front();
     } else {
-        return this.arr[mp];
+        // even length array return average of min and max q 
+        let num1 = this.minQ.front();
+        let num2 = this.maxQ.front();
+        return ((num1 + num2) / 2);
     }
 };
 
