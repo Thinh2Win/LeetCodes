9f97f1da31c53eb41f89f028caa5bd30f0e4fd8c
Thinh Ngo Phan <ngophanthinh@gmail.com>
2025-03-09 16:34:19 -0700

    Time: 71 ms (23.53%), Space: 63.7 MB (17.65%) - LeetHub

--- 0971-shortest-bridge/0971-shortest-bridge.js
+++ 0971-shortest-bridge/0971-shortest-bridge.js
@@ -3,91 +3,74 @@
  * @return {number}
  */
 var shortestBridge = function(grid) {
-    // BFS from smallest island 
-    // traverse grid and find the size of both islands 
-    // BFS + memo to track visited squares to get count 
-    // BFS if we encounter a 1
-    let visited = new Map();
-    let island1 = null;
-    let island2 = null;
-    let map1 = null;
-    let map2 = null;
-
+    let visited = new Map(); // will allow us to track which nodes we've visited and maps which nodes belong to the island 
+    let island = null;
+    // traverse through grid, when we encounter a 1 we bfs to find connecting 1's that form the island 
     for (let row = 0; row < grid.length; row++) {
         for (let col = 0; col < grid[0].length; col++) {
-            if (grid[row][col] === 1 && !visited.has(`${row},${col}`)) {
-                let [island, islandMap] = BFS([row, col], grid, visited);
-                if (!island1) {
-                    island1 = island;
-                    map1 = islandMap;
-                } else {
-                    island2 = island;
-                    map2 = islandMap;
+            if (island) break;
+            if (grid[row][col] === 1) {
+                island = BFS([row, col], grid, visited);
+                break;
+            }
+        }
+    }
+    // keep track of the squares in a map and in a queue 
+    let queue = island;
+    let directions = [[1,0],[-1,0],[0,1],[0,-1]];
+    let rounds = 0;
+    // once we found 1 island, we BFS from the squares 
+    while (queue.length) {
+        let neighbors = [];
+        let end = queue.length;
+        for (let i = 0; i < end; i++) {
+            let [row, col] = queue.pop();
+            for (let [x, y] of directions) {
+                let newRow = row + x;
+                let newCol = col + y;
+                let path = `${newRow},${newCol}`;
+                if (grid?.[newRow]?.[newCol] === 1 && !visited.get(path)) {
+                    // islands are connected return count
+                    return rounds;
+                } else if (grid?.[newRow]?.[newCol] === 0 && !visited.get(path)) {
+                    // bfs to find other island 
+                    visited.set(path, true);
+                    neighbors.push([newRow, newCol]);
                 }
             }
         }
+        queue = neighbors;
+        rounds += 1;
     }
-    let smallest = island1.length < island2.length ? island1 : island2;
-    let sMap = smallest === island1 ? map1 : map2;
-    let lMap = smallest === island2 ? map1 : map2;
-    let queue = smallest;
-    let count = 0;
-    while (queue.length) {
-        let directions = [[1,0],[-1,0],[0,1],[0,-1]];
-         let nextRound = [];
-    // pop from queue and increment count 
-        let end = queue.length;
-        for (let i = 0; i < end; i++) {
-            let [row, col] = queue.pop();
-            sMap.set(`${row},${col}`, true);
-            for (let [x, y] of directions) {
-                let newRow = row + x;
-                let newCol = col + y;
-                let check = `${newRow},${newCol}`;
-                if (lMap.has(check)) return count;
-    // push any neighbor 1's into queue if we havent checked them already 
-                if (grid?.[newRow]?.[newCol] === 0 && !sMap.get(check)) {
-                    sMap.set(check, true);
-                    nextRound.push([newRow, newCol]);
-                } 
-            }
-        }
-        count += 1;
-        queue = nextRound;
-    }
+    return rounds;
+    // keep track of how many bfs rounds we've done 
+    // if we reach another square that is not apart of our curr island we can return our count 
 };
 
 function BFS(square, grid, visited) {
-    // count 
-    let island = [];
-    let islandMap = new Map();
-    // queue to hold 1's 
     let queue = [square];
+    let island = [];
     let directions = [[1,0],[-1,0],[0,1],[0,-1]];
 
-    while (queue.length) {
-    // 2nd queue to hold next round of bfs 
-        let nextRound = [];
-    // pop from queue and increment count 
+    while (queue.length) {  
+        let neighbors = [];
         let end = queue.length;
+        island.push(...queue);
         for (let i = 0; i < end; i++) {
             let [row, col] = queue.pop();
-            island.push([row, col]);
             visited.set(`${row},${col}`, true);
-            islandMap.set(`${row},${col}`, true);
             for (let [x, y] of directions) {
                 let newRow = row + x;
                 let newCol = col + y;
-                let check = `${newRow},${newCol}`;
-    // push any neighbor 1's into queue if we havent checked them already 
-                if (grid?.[newRow]?.[newCol] === 1 && !visited.get(check)) {
-                    visited.set(check, true);
-                    nextRound.push([newRow, newCol]);
-                } 
+                let path = `${newRow},${newCol}`;
+                if (grid?.[newRow]?.[newCol] === 1 && !visited.get(path)) {
+                     visited.set(path, true);
+                     neighbors.push([newRow, newCol]);
+                }
             }
         }
-    // set queue to 2nd queue to start next round 
-        queue = nextRound;
+        queue = neighbors;
     }
-    return [island, islandMap];
+   
+    return island;
 }
