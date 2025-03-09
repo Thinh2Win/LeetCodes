89e50f22f7ec3d2ad70d87d8ef53e1f01e22a7d6
Thinh Ngo Phan <ngophanthinh@gmail.com>
2025-03-09 10:57:59 -0700

    Time: 454 ms (7.77%), Space: 85.6 MB (5.11%) - LeetHub

---
+++ 2487-optimal-partition-of-string/2487-optimal-partition-of-string.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var partitionString = function(s) {
+  // abacaba -> ab ac ab a
+  // sliding window 
+  // keep track of letters inside window 
+  // when we reach a dupe we move the L pointer to R 
+  // increase count to represent substrings 
+    let freq = {};
+    let count = 0;
+    let L = 0;
+    for (let R = 0; R < s.length; R++) {
+        let char = s[R];
+        freq[char] ? freq[char] += 1 : freq[char] = 1;
+        if (freq[char] >= 2) {
+            L = R;
+            freq = {[char]: 1};
+            count += 1;
+        };
+    } 
+    // count substring at end
+    return count + 1;
+};
