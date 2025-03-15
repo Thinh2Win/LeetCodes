75a432a2a5ae0f20e4120f17f08bf3ca5015fa14
Thinh Ngo Phan <ngophanthinh@gmail.com>
2025-03-15 14:30:11 -0700

    Time: 0 ms (100%), Space: 54.7 MB (20.42%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -4,29 +4,30 @@
  * @return {number[]}
  */
 var searchRange = function(nums, target) {
-    let answer = [-1, -1];
-    let L = 0;
-    let R = nums.length - 1;
-    while (L < R) {
-        let mp = Math.floor((R - L) / 2) + L;
+    let L = 0; 
+    let R = nums.length; 
+    let answer = [-1, -1]
+    while (L <= R) {
+        let mp = Math.floor((R - L) / 2 + L);
+
         if (nums[mp] >= target) {
-            R = mp;
+            R = mp - 1;
         } else {
-            L = mp + 1;
+            L = mp + 1
         }
     }
-    if (nums[L] !== target) return answer;
+    if (nums[L] !== target) return answer; 
     answer[0] = L;
+    R = nums.length; 
+    while (L <= R) {
+        let mp = Math.floor((R - L) / 2 + L);
 
-    R = nums.length - 1;
-    while (L < R) {
-        let mp = Math.floor((R - L) / 2) + L;
         if (nums[mp] <= target) {
             L = mp + 1;
         } else {
-            R = mp;
+            R = mp - 1;
         }
     }
-    answer[1] = nums[L] === target ? L : L - 1;
+    answer[1] = nums[R] === target ? R : answer[0];
     return answer;
 };
