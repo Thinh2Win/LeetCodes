675aaaa2d66c9672c5fe1ab77809f98f33ea3e98
Thinh Ngo Phan <ngophanthinh@gmail.com>
2025-03-31 16:20:53 -0700

    Time: 26 ms (47.87%), Space: 80.6 MB (25.9%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.js
+++ 0739-daily-temperatures/0739-daily-temperatures.js
@@ -3,19 +3,22 @@
  * @return {number[]}
  */
 var dailyTemperatures = function(temperatures) {
-    let stack = [[Infinity]];
+    // have a stack to track temperatures by idxs
+    // loop through temperatures and check if the curr temp is greater than top of stack 
+    // if so we pop from stack to get the idx, and set an answer array at that idx = curr temp idx - popped idx 
+    let stack = [];
     let answer = new Array(temperatures.length).fill(0);
     for (let i = 0; i < temperatures.length; i++) {
+        if (stack.length === 0) {
+            stack.push(i);
+            continue;
+        };
         let currTemp = temperatures[i];
-        if (currTemp <= stack.at(-1)[0]) {
-            stack.push([currTemp, i]);
-        } else {
-            while (currTemp > stack.at(-1)[0]) {
-                let [pastTemp, idx] = stack.pop();
-                answer[idx] = i - idx;
-            }
-            stack.push([currTemp, i]);
+        while (currTemp > temperatures[stack.at(-1)]) {
+            let idx = stack.pop();
+            answer[idx] = i - idx;
         }
+        stack.push(i);
     }
     return answer;
 };
