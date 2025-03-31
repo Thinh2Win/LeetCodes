41e0d9cac61a08de304fcd3404cf94db2f56ce8c
Thinh Ngo Phan <ngophanthinh@gmail.com>
2025-03-30 17:39:52 -0700

    Time: 0 ms (100%), Space: 53.5 MB (49.71%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.js
+++ 0062-unique-paths/0062-unique-paths.js
@@ -4,19 +4,23 @@
  * @return {number}
  */
 var uniquePaths = function(m, n) {
-    let matrix = new Array(m).fill(0).map((zero, idx) => {
-        if (idx === 0) {
-            return new Array(n).fill(1);
-        } else {
-            let row = new Array(n).fill(0);
-            row[0] = 1;
-            return row;
-        }
+    // since the robot can only come from 2 directions 
+    // work backwards from the end 
+    // from the end we can only reach it from the top square, or left square 
+    // set those as 1 
+    // we can calculate the total unique paths of a square by adding the total of its bottom square and right square
+    const dp = Array.from({length: m - 1}, () => {
+        let arr = new Array(n).fill(0);
+        arr[arr.length - 1] = 1;
+        return arr;
     });
-    for (let row = 1; row < m; row++) {
-        for (let col = 1; col < n; col++) {
-            matrix[row][col] = matrix[row - 1][col] + matrix[row][col - 1];
+    // fill bottom row with 1's 
+    dp.push(new Array(n).fill(1));
+    // starting from sencond to last row, iterate backwards and calculate square by adding the square to its right and bottom
+    for (let row = m - 2; row >= 0; row--) {
+        for (let col = n - 2; col >= 0; col--) {
+            dp[row][col] = dp[row + 1][col] + dp[row][col + 1]
         }
-    }
-    return matrix.at(-1).at(-1);
+    } 
+    return dp[0][0];
 };
