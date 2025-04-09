f454c1d21281ceec21dd6dafa50919254fc000b1
Thinh Ngo Phan <ngophanthinh@gmail.com>
2025-04-09 09:37:57 -0700

    Time: 14 ms (60.89%), Space: 60.2 MB (75.81%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.js
+++ 0207-course-schedule/0207-course-schedule.js
@@ -4,42 +4,51 @@
  * @return {boolean}
  */
 var canFinish = function(numCourses, prerequisites) {
-   // kahns algo to detect cycles 
-   // need an adjecency list 
-   // need an inDegree array where the course matches the arrays index and the value is a number that keeps track of the pre reqs are needed to complete that course 
-   // prerequisites = [[1,0], [0, 2], [2, 3], [0, 3]]
-   // for example course 0 needing course 2 and 3 would show [2, 1, 1, 0]
-   // courses that require 0 pre reqs are placed in a queue
-   // utilizing our adj list we can work backwards and decrement courses that need course 3 
-   // any courses with 0 in degree will be added to the queue
-   // keep count every time we remove a course from the queue 
-   // if the count === numCourses we know we can complete all the courses
+    /*
+        kahns algo
+        keep track of the number of courses that depend on the preReq 
+        ex. [[2,0], [1,0], [3,0]]
+        there are 3 courses dependent on course 0 
+        [3, 0, 0, 0] <- idx correlates with course number, value = dependent courses 
+        for each course that is a 0, we can add to a queue 
+        we pop the course from the queue and check which course was a pre req for the current 
+        decrement the prereq from the array 
+        if any values reach 0 we push into the queue 
+        ex. courses 1, 2, 3 would be in the queue 
+        0 was a prereq for course 1 so we decrement course 0 
+        [2, 0, 0, 0]
+        do the same for the others 
+        [0, 0, 0, 0]
+        if by the end our queue is empty and all the values are 0 we know we can complete the courses 
+        ex. when we cant [[2,0], [1,0], [3,0], [0,2]]
+        [3, 0, 1, 0]
+        queue has course 1 and 3
+        0 is a prereq of both so we decrement 
+        [1, 0, 1, 0]
+        queue is empty with no new values and our array still contains values other than 0 so we know theres a
+        loop 
+    */
 
+    const adjList = Array.from({length: numCourses}, () => []);
+    const dependants = new Array(numCourses).fill(0);
 
-    let adjList = new Array(numCourses).fill(0).map(zero => []);
-    let inDegree = new Array(numCourses).fill(0);
-    prerequisites.forEach(pair => {
-        let [course, prereq] = pair;
-        adjList[prereq].push(course);
-        inDegree[course] += 1;
-    });
+    for (let [c1, c2] of prerequisites) {
+        adjList[c1].push(c2); // create relationship of courses that rely on curr course
+        dependants[c2] += 1;
+    }
 
     let queue = [];
-    inDegree.forEach((count, idx) => {
-        if (count === 0) queue.push(idx);
-    });
-
-    let count = 0;
+    for (let course = 0; course < dependants.length; course++) {
+        if (dependants[course] === 0) queue.push(course);
+    };
+    
     while (queue.length) {
-        let course = queue.shift();
-        count += 1;
-
-        let neighbors = adjList[course];
-        neighbors.forEach(neighbor => {
-            inDegree[neighbor] -= 1;
-            if (inDegree[neighbor] === 0) queue.push(neighbor);
-        });
+        let course = queue.pop();
+        let prereqs = adjList[course];
+        for (let prereq of prereqs) {
+            dependants[prereq] -= 1;
+            if (dependants[prereq] === 0) queue.push(prereq);
+        }
     }
-    return count === numCourses;
+    return dependants.every(num => num === 0);
 };
-
