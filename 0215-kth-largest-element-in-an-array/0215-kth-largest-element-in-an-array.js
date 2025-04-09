dce428e3cd359ac1effee0efc7b9b50cbafe4eb9
Thinh Ngo Phan <ngophanthinh@gmail.com>
2025-04-09 09:15:46 -0700

    Time: 64 ms (62.18%), Space: 71.8 MB (25.99%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
@@ -5,7 +5,11 @@
  */
 var findKthLargest = function(nums, k) {
     const maxQ = new MaxPriorityQueue();
-    for (let num of nums) maxQ.enqueue(num);
-    for (let i = 1; i < k; i++) maxQ.dequeue();
-    return maxQ.front();
+    for (let num of nums) {
+        maxQ.enqueue(num);
+    }
+    for (let i = 1; i <= k; i++) {
+        if (i === k) return maxQ.dequeue();
+        maxQ.dequeue();
+    }
 };
