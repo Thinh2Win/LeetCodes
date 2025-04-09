502894ebdef6951ae64d6ed44181171a1b0d26f5
Thinh Ngo Phan <ngophanthinh@gmail.com>
2025-04-09 08:02:40 -0700

    Time: 45 ms (91.5%), Space: 59.1 MB (51.42%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -13,12 +13,11 @@
 var hasCycle = function(head) {
     let slow = head;
     let fast = head;
+
     while (fast) {
         slow = slow.next;
         fast = fast?.next?.next;
-        if (slow === fast) {
-            return true;
-        }
+        if (slow === fast) return true;
     }
     return false;
 };
