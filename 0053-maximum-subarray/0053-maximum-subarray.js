8de5ce7cd876302b469e6867dd423ea8213a864b
Thinh Ngo Phan <ngophanthinh@gmail.com>
2025-04-09 08:55:41 -0700

    Time: 6 ms (13.5%), Space: 65.5 MB (15.19%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -3,14 +3,16 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    let max = -Infinity;
+    // keep track of the max sum
+    // keep track of curr sum 
+    // loop through nums and add to curr sum 
+    // if curr num >= curr sum reset curr sum to the num 
+    let maxSum = -Infinity;
     let currSum = 0;
-    for (let i = 0; i < nums.length; i++) {
-        currSum += nums[i];
-        if (currSum < nums[i]) {
-            currSum = nums[i];
-        }
-        max = Math.max(max, currSum);
-    };
-    return max;
+    for (let num of nums) {
+        currSum += num;
+        if (num >= currSum) currSum = num;
+        maxSum = Math.max(maxSum, currSum);
+    }
+    return maxSum;
 };
