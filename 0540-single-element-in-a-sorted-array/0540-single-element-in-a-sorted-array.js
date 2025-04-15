d2e2e24ec0707a9a6fbfa5d88b715e8f07a88cb5
Thinh Ngo Phan <ngophanthinh@gmail.com>
2025-04-15 13:13:13 -0700

    Time: 0 ms (100%), Space: 57 MB (62.65%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.js
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.js
@@ -20,24 +20,21 @@ var singleNonDuplicate = function(nums) {
     let L = 0;
     let R = nums.length;
 
-    while (nums.length > 1) {
-        let L = 0;
-        let R = nums.length;
+    while (L < R) {
         let mp = Math.floor((R - L) / 2 + L);
-        let dupeIdx = null;
 
         if (nums[mp] === nums[mp + 1]) {
-            dupeIdx = mp + 1;
+            let left = mp - L; 
+            let right = R - mp; // would add 1 cause dupe is on right side but we subtract 1 anyway cause 0 idx
+            left % 2 === 1 ? R = mp - 1 : L = mp + 2;
         } else if (nums[mp] === nums[mp - 1]) {
-            dupeIdx = mp - 1;
+            let left = mp - 1 - L; 
+            let right = R - mp - 1;
+            left % 2 === 1 ? R = mp - 1 : L = mp + 1;
         } else {
             // no dupes 
             return nums[mp];
         }
-
-        let left = nums.slice(0, mp > dupeIdx ? dupeIdx : mp);
-        let right = nums.slice((mp > dupeIdx ? mp : dupeIdx) + 1, nums.length);
-        nums = left.length % 2 === 1 ? left : right;
     }
-    return nums[0];
+    return nums[L];
 };
