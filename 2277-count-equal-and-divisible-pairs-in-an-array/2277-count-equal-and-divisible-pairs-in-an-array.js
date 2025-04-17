c2a113707c1d2065eaa3e7db72b2c2f957988194
Thinh Ngo Phan <ngophanthinh@gmail.com>
2025-04-16 20:03:39 -0700

    Time: 3 ms (62.45%), Space: 55.8 MB (17.47%) - LeetHub

---
+++ 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var countPairs = function(nums, k) {
+    let answer = 0;
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] === nums[j] && ((i * j) % k === 0)) answer += 1;
+        }
+    }
+    return answer;
+};
