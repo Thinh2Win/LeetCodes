8498ccdbe4afa4a3fe6ff6e928d38a8d14b58556
Thinh Ngo Phan <ngophanthinh@gmail.com>
2025-04-17 19:53:44 -0700

    Time: 7 ms (34.47%), Space: 57.9 MB (17.11%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.js
+++ 0038-count-and-say/0038-count-and-say.js
@@ -2,30 +2,19 @@
  * @param {number} n
  * @return {string}
  */
-let map = {
-    1: '1',
-    2: '11',
-    3: '21'
-}
-var countAndSay = function(n) {
-    if (map[n]) return map[n];
-    let i = 3;
-    while (i < n) {
-        let string = '';
-        let curr = map[i];
-        let idx = 0;
+var countAndSay = function(n, string = '1', times = 1) {
+    if  (times === n) return string;
+    let characters = string.split('');
+    let i = 0;
+    let answer = [];
+    while (i < string.length) {
         let count = 1;
-        while (idx < curr.length) {
-            if (curr[idx + 1] === curr[idx]) {
-                count += 1;
-            } else {
-                string += `${count}${curr[idx]}`;
-                count = 1;
-            }
-            idx += 1;
+        while (string[i + 1] === string[i]) {
+            i += 1;
+            count += 1;
         }
-        map[i + 1] = string;
+        answer.push(count, string[i]);
         i += 1;
     }
-    return map[n];
+    return countAndSay(n, answer.join(''), times + 1);
 };
