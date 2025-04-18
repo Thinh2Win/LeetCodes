7b3704dfe4ce673ee0e8758d4e5b7eb6488e58e1
Thinh Ngo Phan <ngophanthinh@gmail.com>
2025-04-18 09:46:11 -0700

    Time: 8 ms (87.31%), Space: 57.5 MB (74.62%) - LeetHub

--- 0696-count-binary-substrings/0696-count-binary-substrings.js
+++ 0696-count-binary-substrings/0696-count-binary-substrings.js
@@ -3,42 +3,17 @@
  * @return {number}
  */
 var countBinarySubstrings = function(s) {
-    /*
-        00001111 total = 4
-        000111
-        0011
-        01
-
-        as we loop through s 
-        we keep track of 
-            - current number 
-            - current number of 0's 
-            - current number of 1's 
-        so when we reach a number thats not our current, we have alternated 
-            so we add the min of current 1's or 0's to our answer count 
-            reset opposite count 
-    */  
     let answer = 0;
-    let curr = s[0];
-    let currCount = 0;
-    let i = 0;
-    while (i < s.length) {
-        let num = s[i];
-        if (num !== curr) {
-            let numCount = 1;
-            let j = i;
-            while (s[j] === s[j + 1]) {
-                j += 1;
-                numCount += 1;
-            }
-            answer += Math.min(currCount, numCount);
-            currCount = numCount
-            curr = s[j];
-            i = j;
+    let prev = 0;
+    let curr = 1;
+    for (let i = 1; i < s.length; i++) {
+        if (s[i] === s[i - 1]) {
+            curr += 1;
         } else {
-            currCount += 1;
+            answer += Math.min(curr, prev);
+            prev = curr;
+            curr = 1;
         }
-        i += 1;
     }
-    return answer;
+    return answer += Math.min(curr, prev);
 };
