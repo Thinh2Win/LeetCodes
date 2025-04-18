717ca1c0f21a923c6465efa1fa2403c4c93f009b
Thinh Ngo Phan <ngophanthinh@gmail.com>
2025-04-18 13:31:05 -0700

    Time: 6 ms (38.24%), Space: 56.2 MB (32.35%) - LeetHub

--- 1096-maximum-sum-of-two-non-overlapping-subarrays/1096-maximum-sum-of-two-non-overlapping-subarrays.js
+++ 1096-maximum-sum-of-two-non-overlapping-subarrays/1096-maximum-sum-of-two-non-overlapping-subarrays.js
@@ -5,21 +5,29 @@
  * @return {number}
  */
 var maxSumTwoNoOverlap = function(nums, firstLen, secondLen) {
+    /*
+        Prefix Sum array to help us quickly find sum of a subarray 
+    */
     let preSum = new Array(nums.length + 1).fill(0);
     for (let i = 0; i < nums.length; i++) {
+        let num = nums[i];
         preSum[i + 1] = preSum[i] + nums[i];
     }
-    const subSum = (i, j) => preSum[i + j] - preSum[i];
+
+    function calcSum(i, j) {
+        return preSum[j] - preSum[i];
+    }
 
     function best(m, n) {
         let bestM = 0;
-        let ans = 0;
+        let answer = 0;
         for (let i = m; i + n <= nums.length; i++) {
-            bestM = Math.max(bestM, subSum(i - m, m));
-            ans = Math.max(ans, bestM + subSum(i, n))
+            bestM = Math.max(bestM, calcSum(i - m, m + i - m));
+            answer = Math.max(answer, bestM + calcSum(i, n + i));
         }
-        return ans;
+        return answer;
     }
+
     let best1 = best(firstLen, secondLen);
     let best2 = best(secondLen, firstLen);
     return Math.max(best1, best2);
