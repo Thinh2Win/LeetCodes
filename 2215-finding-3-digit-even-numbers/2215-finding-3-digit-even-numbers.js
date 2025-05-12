124a6a2db1f9203eabd91c88f892155f782f482f
Thinh Ngo Phan <ngophanthinh@gmail.com>
2025-05-12 08:06:25 -0700

    Time: 31 ms (66.38%), Space: 61.3 MB (18.1%) - LeetHub

--- 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.js
+++ 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.js
@@ -3,26 +3,22 @@
  * @return {number[]}
  */
 var findEvenNumbers = function(digits) {
-    let answer = new Set();
-    let visited = new Array(digits.length).fill(0);
-
-    const helper = (nums, container) => {
-        if (container.length >= 3) {
-            let number = +container.join('');
-            if (number % 2 === 0) answer.add(number);
-            return
-        };
-
-        for (let i = 0; i <nums.length; i++) {
-            let num = nums[i];
-            if ((!container.length && num == 0) || visited[i]) continue;
-            visited[i] = 1;
-            container.push(num);
-            helper(nums, container);
-            container.pop(num);
-            visited[i] = 0;
-        }
-    };
-    helper(digits, []);
-    return Array.from(answer).sort((a, b) => a - b);
+    const answer = [];
+    for (let i = 100; i <= 998; i += 2) {
+        if (canBuild(i, digits)) answer.push(i);
+    }
+    return answer;
 };
+
+function canBuild(num, digits) {
+    let map = {};
+    digits.forEach(num => map[num] = (map[num] || 0) + 1 );
+    let string = `${num}`
+    for (let i = 0; i < string.length; i++) {
+        let char = string[i];
+        if (!map[char]) return false;
+        map[char] -= 1;
+        if (map[char] < 0) return false;
+    }
+    return true;
+}
