5a963dd1c67f77de145c7c78dfb72402958a540b
Thinh Ngo Phan <ngophanthinh@gmail.com>
2025-05-12 17:32:42 -0700

    Time: 41 ms (19.73%), Space: 65.7 MB (25.34%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.js
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.js
@@ -7,17 +7,27 @@
  * @return {number}
  */
 var findCheapestPrice = function(n, flights, src, dst, k) {
-    let dp = new Array(n).fill(Infinity);
-    dp[src] = 0;
-    for (let i = 0; i <= k; i++) {
-        let temp = [...dp];
-        for (let [start, end, cost] of flights) {
-            if (dp[start] !== Infinity) {
-                // possible to fly 
-                temp[end] = Math.min(temp[end], dp[start] + cost);
-            }
-        }
-        dp = temp;
+    const dp = Array.from({length: n}, () => new Array(k + 2).fill(Infinity));
+    dp[src][k + 1] = 0;
+
+    const adjList = Array.from({length: n}, () => []);
+    flights.forEach(([from, to, cost]) => adjList[from].push([to, cost]));
+
+    const minQ = new MinPriorityQueue(duple => duple[1]);
+    minQ.enqueue([src, 0, k + 1]);
+  
+    while(minQ.size()) {
+        let [node, currCost, stops] = minQ.dequeue();
+        if (node === dst) return currCost;
+
+        let cities = adjList[node];
+        cities.forEach(([city, cost]) => {
+            let totalCost = cost + currCost; 
+            if (dp[city][stops] > totalCost && stops > 0) {
+                minQ.enqueue([city, totalCost, stops - 1]);
+                dp[city][stops] = totalCost;
+            };
+        });
     }
-    return dp[dst] === Infinity ? -1 : dp[dst];
+    return -1;
 };
