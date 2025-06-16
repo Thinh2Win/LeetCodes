6a939bb7a1b4694ea535f52418cab4fe3f0541c2
Thinh Ngo Phan <ngophanthinh@gmail.com>
2025-06-16 16:43:57 -0700

    Time: 0 ms (100%), Space: 58.3 MB (38.34%) - LeetHub

--- 1058-lexicographically-smallest-equivalent-string/1058-lexicographically-smallest-equivalent-string.js
+++ 1058-lexicographically-smallest-equivalent-string/1058-lexicographically-smallest-equivalent-string.js
@@ -7,28 +7,33 @@
 var smallestEquivalentString = function(s1, s2, baseStr) {
     const parents = 'abcdefghijklmnopqrstuvwxyz'.split('');
     function find(char) {
-        if (parents[char.charCodeAt(0) - 97] !== char) {
-            parents[char.charCodeAt(0) - 97] = find(parents[char.charCodeAt(0) - 97])
+        let idx = char.charCodeAt(0) - 97;
+        if (char !== parents[idx]) {
+            parents[idx] = find(parents[idx]);
         }
-        return parents[char.charCodeAt(0) - 97];
+        return parents[idx];
     }
-
-    function union(c1, c2) {
-        let p1 = find(c1);
-        let p2 = find(c2);
-
+    function union(n1, n2) {
+        let p1 = find(n1);
+        let p2 = find(n2);
+        let idx1 = p1.charCodeAt(0) - 97;
+        let idx2 = p2.charCodeAt(0) - 97;
         if (p1 !== p2) {
-            if (p1 < p2) parents[p2.charCodeAt(0) - 97] = p1
-            else parents[p1.charCodeAt(0) - 97] = p2
+            if (p1 < p2) parents[idx2] = p1
+            else parents[idx1] = p2;
         }
     }
+
     for (let i = 0; i < s1.length; i++) {
-        union(s1[i], s2[i]);
+       let char1 = s1[i];
+       let char2 = s2[i];
+       union(char1, char2);
     }
 
     let answer = [];
     for (let char of baseStr) {
-        answer.push(find(char));
+        let idx = char.charCodeAt(0) - 97;
+        answer.push(find(parents[idx]));
     }
     return answer.join('');
 };
