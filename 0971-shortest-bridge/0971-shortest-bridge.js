4c6675431a5c5cf76cb7bc15f2cbc3dbf2af8bb0
Thinh Ngo Phan <ngophanthinh@gmail.com>
2025-06-18 13:52:22 -0700

    Time: 37 ms (56.91%), Space: 64.2 MB (54.47%) - LeetHub

--- 0971-shortest-bridge/0971-shortest-bridge.js
+++ 0971-shortest-bridge/0971-shortest-bridge.js
@@ -3,14 +3,13 @@
  * @return {number}
  */
 var shortestBridge = function(grid) {
-    let visited = new Map(); // will allow us to track which nodes we've visited and maps which nodes belong to the island 
     let island = null;
     // traverse through grid, when we encounter a 1 we bfs to find connecting 1's that form the island 
     for (let row = 0; row < grid.length; row++) {
         for (let col = 0; col < grid[0].length; col++) {
             if (island) break;
             if (grid[row][col] === 1) {
-                island = BFS([row, col], grid, visited);
+                island = BFS([row, col], grid);
                 break;
             }
         }
@@ -28,13 +27,12 @@ var shortestBridge = function(grid) {
             for (let [x, y] of directions) {
                 let newRow = row + x;
                 let newCol = col + y;
-                let path = `${newRow},${newCol}`;
-                if (grid?.[newRow]?.[newCol] === 1 && !visited.get(path)) {
+                if (grid?.[newRow]?.[newCol] === 1) {
                     // islands are connected return count
                     return rounds;
-                } else if (grid?.[newRow]?.[newCol] === 0 && !visited.get(path)) {
+                } else if (grid?.[newRow]?.[newCol] === 0 ) {
                     // bfs to find other island 
-                    visited.set(path, true);
+                    grid[newRow][newCol] = 2;
                     neighbors.push([newRow, newCol]);
                 }
             }
@@ -47,10 +45,11 @@ var shortestBridge = function(grid) {
     // if we reach another square that is not apart of our curr island we can return our count 
 };
 
-function BFS(square, grid, visited) {
+function BFS(square, grid) {
     let queue = [square];
     let island = [];
     let directions = [[1,0],[-1,0],[0,1],[0,-1]];
+    grid[square[0]][square[1]] = 2; // set start to 2 to avoid revisiting
 
     while (queue.length) {  
         let neighbors = [];
@@ -58,13 +57,11 @@ function BFS(square, grid, visited) {
         island.push(...queue);
         for (let i = 0; i < end; i++) {
             let [row, col] = queue.pop();
-            visited.set(`${row},${col}`, true);
             for (let [x, y] of directions) {
                 let newRow = row + x;
                 let newCol = col + y;
-                let path = `${newRow},${newCol}`;
-                if (grid?.[newRow]?.[newCol] === 1 && !visited.get(path)) {
-                     visited.set(path, true);
+                if (grid?.[newRow]?.[newCol] === 1) {
+                     grid[newRow][newCol] = 2;
                      neighbors.push([newRow, newCol]);
                 }
             }
