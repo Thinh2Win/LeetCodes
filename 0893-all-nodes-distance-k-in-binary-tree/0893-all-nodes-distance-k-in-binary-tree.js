577e731bc8c07a0da83e9aad9e577021fab31e80
Thinh Ngo Phan <ngophanthinh@gmail.com>
2025-06-18 15:59:32 -0700

    Time: 50 ms (67.24%), Space: 56.6 MB (77.13%) - LeetHub

--- 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.js
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.js
@@ -13,41 +13,45 @@
  */
 var distanceK = function(root, target, k) {
     /*
-        DFS to find target 
-        then BFS with target k times 
-        have a set to track visited nodes 
-        push left, right and parent of target into queue if we have not yet already  
+        BFS 
+        - find target node using DFS 
+            - make sure to track parent when DFS to add parent to node 
+        - once we find target node place node in queue
+        - note: queue should hold [node, prevNode] to avoid revisiting nodes 
+        - keep track of dist 
+        - pop from queue 
+            - push nodes left, right, and parent node if they exist and are not prevNode into queue 
+            - if our dist === k, return queue 
     */
-    if (!root) return root;
-    let curr = root;
-    findTarget(curr, target, null); // connect parents to target
-    let visited = new Set();
-    visited.add(target);
-    let queue = [target];
-    for (let i = 0; i < k; i++) {
+    
+    connectParents(root);
+    let queue = [[target, -1]]; // currNode, prevNode
+    let dist = 0;
+
+    while (queue.length) {
+        let nextRound = [];
         let end = queue.length;
-        for (let j = 0; j < end; j++) {
-            let node = queue.shift();
-            if (node.left && !visited.has(node.left)) {
-                visited.add(node.left);
-                queue.push(node.left);
-            }
-            if (node.right && !visited.has(node.right)) {
-                visited.add(node.right);
-                queue.push(node.right);
-            }
-            if (node.parent && !visited.has(node.parent)) {
-                visited.add(node.parent);
-                queue.push(node.parent);
-            }
+        if (dist === k) return queue.map(node => node[0].val);
+        for (let i = 0; i < end; i++) {
+            let [currNode, prevNode] = queue.pop();
+            if (currNode.left && prevNode !== currNode.left) nextRound.push([currNode.left, currNode]);
+            if (currNode.right && prevNode !== currNode.right) nextRound.push([currNode.right, currNode]);
+            if (currNode.parent && prevNode !== currNode.parent) nextRound.push([currNode.parent, currNode]);
         }
+
+        dist += 1;
+        queue = nextRound;
     }
-    return queue.map(node => node.val);
+    return [];
 };
 
-function findTarget(node, target, parent) {
-    node.parent = parent;
-    if (node === target) return;
-    node.left && findTarget(node.left, target, node);
-    node.right && findTarget(node.right, target, node);
+function connectParents(root) {
+    let tNode = null;
+    
+    const DFS = (node, parent) => {
+        node.parent = parent;
+        node.left && DFS(node.left, node);
+        node.right && DFS(node.right, node);
+    };
+    DFS(root, null);
 }
